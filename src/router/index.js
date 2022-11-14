8b22f1e6daadfb71053d3e627fd6f0d83cc2f1f3
Abhishek P Anil <abhishek@culturecreates.com>
2022-11-14 14:13:01 +0530

    feat: added layout for all the auth pages,forgot password route created,added trailing commas

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { PathName } from '../constants/pathName';
 import Login from '../pages/Login';
+import ForgotPassword from '../pages/ForgotPassword';
 
 export const router = createBrowserRouter([
   {
     path: PathName.Login,
     element: <Login />,
+  },
+  {
+    path: PathName.ForgotPassword,
+    element: <ForgotPassword />,
   },
 ]);
