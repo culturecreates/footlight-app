170c5207cc290a9ffca240c936d18061a8711295
Abhishek P Anil <abhishek@culturecreates.com>
2022-11-14 13:07:03 +0530

    feat: trailing commas added

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ root.render(
         <App />
       </PersistGate>
     </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
