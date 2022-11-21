a2839133904af86b81519095641f64e4d99b63ed
Abhishek P Anil <abhishek@culturecreates.com>
2022-11-21 13:44:20 +0530

    feat: added a sidebar component

---
+++ src/components/Sidebar/Main/Sidebar.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import './sidebar.css';
+import { Layout, Menu } from 'antd';
+import { sidebarItems } from '../../../constants/sidebarItems';
+
+const { Sider } = Layout;
+
+function Sidebar() {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const items = sidebarItems.map((item, index) => {
+    const key = String(index + 1);
+    return {
+      key: key,
+      icon: item.icon,
+      label: item.name,
+    };
+  });
+  return (
+    <Sider
+      width={200}
+      className="sidebar-wrapper"
+      collapsible
+      collapsed={collapsed}
+      onCollapse={(value) => setCollapsed(value)}>
+      <Menu
+        mode="inline"
+        defaultSelectedKeys={['1']}
+        style={{
+          height: '100%',
+          borderRight: 0,
+        }}
+        items={items}
+      />
+    </Sider>
+  );
+}
+
+export default Sidebar;
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,56 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './dashboard.css';
-import { Breadcrumb, Layout, Menu } from 'antd';
+import { Layout } from 'antd';
 import NavigationBar from '../../components/NavigationBar/Dashboard';
-import { sidebarItems } from '../../constants/sidebarItems';
+import Sidebar from '../../components/Sidebar/Main';
 
-const { Header, Content, Sider } = Layout;
-
-const items2 = sidebarItems.map((item, index) => {
-  const key = String(index + 1);
-  return {
-    key: key,
-    icon: item.icon,
-    label: item.name,
-  };
-});
+const { Header, Content } = Layout;
 
 function Dashboard() {
-  const [collapsed, setCollapsed] = useState(false);
   return (
     <Layout>
       <Header className="dashboard-header">
         <NavigationBar />
       </Header>
       <Layout>
-        <Sider
-          width={200}
-          className="site-layout-background"
-          collapsible
-          collapsed={collapsed}
-          onCollapse={(value) => setCollapsed(value)}>
-          <Menu
-            mode="inline"
-            defaultSelectedKeys={['1']}
-            style={{
-              height: '100%',
-              borderRight: 0,
-            }}
-            items={items2}
-          />
-        </Sider>
+        <Sidebar />
         <Layout
           style={{
             padding: '0 24px 24px',
           }}>
-          <Breadcrumb
-            style={{
-              margin: '16px 0',
-            }}>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
-          </Breadcrumb>
           <Content
             className="site-layout-background"
             style={{
