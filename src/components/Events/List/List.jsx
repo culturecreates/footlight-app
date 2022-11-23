fc11ab1378bdbe54f29e8a7e5c9c1b8032fee2f2
Abhishek P Anil <abhishek@culturecreates.com>
2022-11-23 19:47:41 +0530

    feat: event listing component layout added

--- src/components/Events/List/List.jsx
+++ src/components/Events/List/List.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import './list.css';
+import { Row, Col } from 'antd';
 
 function List() {
   return (
-    <Row>
-      <Col span={24}>col</Col>
+    <Row className="event-listing-wrapper">
+      <Col>
+        <img className="event-list-image" />
+      </Col>{' '}
+      <Col>
+        <Row>
+          <Col>date</Col>
+          <Col>events</Col>
+        </Row>
+        <Row>
+          <Col>Title</Col>
+        </Row>
+        <Row>
+          <Col>Place</Col>
+        </Row>
+      </Col>{' '}
+      <Col className="event-info-section">
+        <Row>
+          <Col>empty</Col>
+        </Row>
+        <Row>
+          <Col>status</Col>
+        </Row>
+        <Row>
+          <Col>Created</Col>
+        </Row>
+        <Row>
+          <Col>Updated</Col>
+        </Row>
+      </Col>
+      <Col span={2}>options</Col>
     </Row>
   );
 }
--- src/pages/Dashboard/Events/Events.jsx
+++ src/pages/Dashboard/Events/Events.jsx
@@ -3,8 +3,7 @@ import './events.css';
 import { Col, Row } from 'antd';
 import { useTranslation } from 'react-i18next';
 import EventsSearch from '../../../components/Search/Events/EventsSearch';
-
-// import EventList from '../../../components/Events/List';
+import EventList from '../../../components/Events/List';
 
 function Events() {
   const { t } = useTranslation();
@@ -12,19 +11,21 @@ function Events() {
   return (
     <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} className="events-wrapper">
       <Col span={18} style={{ backgroundColor: 'red' }}>
-        <Row>
+        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
           <Col xs={2} sm={4} md={6} lg={8} xl={10}>
             <h4 className="events-heading">{t('dashboard.events.heading')}</h4>
           </Col>
         </Row>
-        <Row>
-          <Col xs={2} sm={4} md={6} lg={8} xl={10}>
+        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+          <Col xs={24} sm={24} md={12} lg={10} xl={8}>
             <EventsSearch placeholder={t('dashboard.events.searchPlaceholder')} />
           </Col>
         </Row>
-        <Row>
-          <Col xs={2} sm={4} md={6} lg={8} xl={10}>
-            listing
+        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} span={24}>
+          <Col span={24}>
+            {[0, 1, 2, 3, 4].map((index) => {
+              return <EventList key={index} />;
+            })}
           </Col>
         </Row>
       </Col>
