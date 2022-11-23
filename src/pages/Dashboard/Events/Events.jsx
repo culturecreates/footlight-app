71133ca9497e2a9db3e59ff66306b67aaf7440cb
Abhishek P Anil <abhishek@culturecreates.com>
2022-11-23 15:49:48 +0530

    feat: added events heading

--- src/pages/Dashboard/Events/Events.jsx
+++ src/pages/Dashboard/Events/Events.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import './events.css';
 import { Col, Row } from 'antd';
+import { useTranslation } from 'react-i18next';
+
 // import EventList from '../../../components/Events/List';
 
 function Events() {
+  const { t } = useTranslation();
+
   return (
-    <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+    <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} className="events-wrapper">
       <Col span={18} style={{ backgroundColor: 'red' }}>
         <Row>
           <Col xs={2} sm={4} md={6} lg={8} xl={10}>
-            Heading
+            <h4 className="events-heading">{t('dashboard.events.heading')}</h4>
           </Col>
         </Row>
         <Row>
