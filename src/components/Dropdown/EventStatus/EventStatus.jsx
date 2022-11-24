fb2bd086217eb78d27bbeb60118a8c55c370455d
Abhishek P Anil <abhishek@culturecreates.com>
2022-11-25 01:11:15 +0530

    feat: constant file added for event publish options

--- src/components/Dropdown/EventStatus/EventStatus.jsx
+++ src/components/Dropdown/EventStatus/EventStatus.jsx
@@ -1,26 +1,18 @@
 import React from 'react';
 import { Dropdown } from 'antd';
 import { useTranslation } from 'react-i18next';
+import { eventPublishOptions } from '../../../constants/eventPublishOptions';
 import './eventStatus.css';
 
 function EventStatusOptions({ children }) {
   const { t } = useTranslation();
-
-  const items = [
-    {
-      label: t('dashboard.events.publishOptions.publishEvent'),
-      key: '0',
-    },
-    {
-      label: t('dashboard.events.publishOptions.unpublishEvent'),
-      key: '1',
-    },
-
-    {
-      label: t('dashboard.events.publishOptions.deleteEvent'),
-      key: '2',
-    },
-  ];
+  const items = eventPublishOptions.map((item, index) => {
+    const key = String(index + 1);
+    return {
+      key: key,
+      label: t(item.label),
+    };
+  });
   return (
     <Dropdown
       className="calendar-dropdown-wrapper"
---
+++ src/constants/eventPublishOptions.js
@@ -0,0 +1,15 @@
+export const eventPublishOptions = [
+  {
+    label: 'dashboard.events.publishOptions.publishEvent',
+    key: '0',
+  },
+  {
+    label: 'dashboard.events.publishOptions.unpublishEvent',
+    key: '1',
+  },
+
+  {
+    label: 'dashboard.events.publishOptions.deleteEvent',
+    key: '2',
+  },
+];
