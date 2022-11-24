6356244ed6d43c34d292ee7b8c33ca74c7c37164
Abhishek P Anil <abhishek@culturecreates.com>
2022-11-25 00:42:23 +0530

    feat: calendar dropdown onclick added

--- src/components/Dropdown/Calendar/Calendar.jsx
+++ src/components/Dropdown/Calendar/Calendar.jsx
@@ -3,25 +3,58 @@ import './calendar.css';
 import { Dropdown } from 'antd';
 
 function Calendar({ children }) {
+  const sampleImage = (
+    <img
+      style={{
+        width: '32px',
+        height: '32px',
+        borderRadius: '73px',
+      }}
+      src={require('../../../assets/images/logo-tout-culture.png')}
+    />
+  );
   const items = [
     {
-      label: <a href="https://www.antgroup.com">1st menu item</a>,
+      label: (
+        <span className="calendar-name-wrapper">
+          <span className="calendar-name">Signé Laval</span>
+          <span className="calendar-organisation-details">6 members </span>
+        </span>
+      ),
       key: '0',
+      icon: sampleImage,
     },
     {
-      label: <a href="https://www.aliyun.com">2nd menu item</a>,
+      label: (
+        <span className="calendar-name-wrapper">
+          <span className="calendar-name">Signé Laval</span>
+          <span className="calendar-organisation-details">6 members </span>
+        </span>
+      ),
       key: '1',
+      icon: sampleImage,
     },
 
     {
-      label: '3rd menu item',
+      label: (
+        <span className="calendar-name-wrapper">
+          <span className="calendar-name">Signé Laval</span>
+          <span className="calendar-organisation-details">6 members </span>
+        </span>
+      ),
       key: '3',
+      icon: sampleImage,
     },
   ];
+  const onClick = ({ key }) => {
+    //Set params after dashboard in route for the selected calendar
+    console.log(key);
+  };
   return (
     <Dropdown
       menu={{
         items,
+        onClick,
       }}
       trigger={['click']}>
       {children}
