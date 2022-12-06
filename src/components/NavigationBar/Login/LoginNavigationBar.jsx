cb6de308f8c1ef113610959865f66afcc71c25e9
Abhishek P Anil <abhishek@culturecreates.com>
2022-12-06 18:22:50 +0530

    fix: locale support constants added to constants folder

--- src/components/NavigationBar/Login/LoginNavigationBar.jsx
+++ src/components/NavigationBar/Login/LoginNavigationBar.jsx
@@ -3,21 +3,22 @@ import './loginNavigationBar.css';
 import OutlinedButton from '../../Button/Outlined';
 import { setInterfaceLanguage } from '../../../redux/reducer/interfaceLanguageSlice';
 import { useDispatch } from 'react-redux';
+import { locale } from '../../../constants/localeSupport';
 import i18n from 'i18next';
 
 function NavigationBar() {
   const dispatch = useDispatch();
-  const [changeLanguageTo, setchangeLanguageTo] = useState('Français');
+  const [changeLanguageTo, setchangeLanguageTo] = useState(locale.FRENCH.label);
 
   const changeLanguageHandler = (event) => {
-    if (event.target.outerText === 'Français') {
-      dispatch(setInterfaceLanguage('fr'));
-      i18n.changeLanguage('fr');
-      setchangeLanguageTo('English');
+    if (event.target.outerText === locale.FRENCH.label) {
+      dispatch(setInterfaceLanguage(locale.FRENCH.key));
+      i18n.changeLanguage(locale.FRENCH.key);
+      setchangeLanguageTo(locale.ENGLISH.label);
     } else {
-      dispatch(setInterfaceLanguage('en'));
-      i18n.changeLanguage('en');
-      setchangeLanguageTo('Français');
+      dispatch(setInterfaceLanguage(locale.ENGLISH.key));
+      i18n.changeLanguage(locale.ENGLISH.key);
+      setchangeLanguageTo(locale.FRENCH.label);
     }
   };
   return (
