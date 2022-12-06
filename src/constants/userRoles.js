6a6a7d5f2fdfa920ac92bed57e60ebcbc5280bb5
Abhishek P Anil <abhishek@culturecreates.com>
2022-12-06 18:16:10 +0530

    feat: user roles file added to the constants folder

---
+++ src/constants/userRoles.js
@@ -0,0 +1,7 @@
+export const Roles = ['ADMIN', 'EDITOR', 'CONTRIBUTOR', 'GUEST'];
+export const userRoles = {
+  ADMIN: 'ADMIN',
+  EDITOR: 'EDITOR',
+  CONTRIBUTOR: 'CONTRIBUTOR',
+  GUEST: 'GUEST',
+};
