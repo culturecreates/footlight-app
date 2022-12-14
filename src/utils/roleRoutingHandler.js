a4995ae6faa0bcf59222d3eac4b74bc17102a2dc
Abhishek P Anil <abhishek@culturecreates.com>
2022-12-14 17:57:13 +0530

    feat: if guest event is published he is routed to read only page

--- src/utils/roleRoutingHandler.js
+++ src/utils/roleRoutingHandler.js
@@ -1,14 +1,17 @@
+import { eventPublishState } from '../constants/eventPublishState';
 import { userRoles } from '../constants/userRoles';
 
-export const routinghandler = (user, calendarId, creatorId) => {
+export const routinghandler = (user, calendarId, creatorId, publishState = '') => {
   const calendar = user?.roles.filter((calendar) => {
     return calendar.calendarId === calendarId;
   });
 
   switch (calendar[0]?.role) {
     case userRoles.GUEST:
-      if (user?.id === creatorId) return true;
-      else return false;
+      if (user?.id === creatorId) {
+        if (eventPublishState.PUBLISHED === publishState) return false;
+        else return true;
+      } else return false;
     case userRoles.CONTRIBUTOR:
       if (user?.id === creatorId) return true;
       else return false;
