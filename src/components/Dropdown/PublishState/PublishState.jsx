cda214a5510395689acc76c917db94077fd20d7c
Abhishek P Anil <abhishek@culturecreates.com>
2022-12-16 15:10:10 +0530

    feat: css for publish state dropdown button added

--- src/components/Dropdown/PublishState/PublishState.jsx
+++ src/components/Dropdown/PublishState/PublishState.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Dropdown } from 'antd';
-// import { useTranslation } from 'react-i18next';
+import './publishState.css';
+import { Button, Dropdown, Space } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
 import { eventPublishState } from '../../../constants/eventPublishState';
 import { useUpdateEventStateMutation } from '../../../services/events';
 import { useParams } from 'react-router-dom';
 function PublishState({ children, eventId }) {
-  //   const { t } = useTranslation();
   const { calendarId } = useParams();
   const [updateEventState] = useUpdateEventStateMutation();
   const items = [
@@ -25,12 +25,18 @@ function PublishState({ children, eventId }) {
 
   return (
     <Dropdown
+      overlayClassName="publish-state-dropdown"
       menu={{
         items,
         onClick,
       }}
       trigger={['click']}>
-      {children}
+      <Button type="default" className="publish-state-dropdown-button">
+        <Space>
+          {children}
+          <DownOutlined style={{ fontSize: '9px', color: '#646D7B' }} />
+        </Space>
+      </Button>
     </Dropdown>
   );
 }
