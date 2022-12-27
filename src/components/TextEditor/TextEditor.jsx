036d17eae62e0dea992ff2775df99a3c437eb560
Abhishek P Anil <abhishek@culturecreates.com>
2022-12-27 17:39:15 +0530

    feat: added the font,alignment format to editor

--- src/components/TextEditor/TextEditor.jsx
+++ src/components/TextEditor/TextEditor.jsx
@@ -6,10 +6,15 @@ import 'react-quill/dist/quill.snow.css';
 
 function TextEditor(props) {
   const { formName, initialValue, dependencies, rules, currentReactQuillRef, placeholder } = props;
-
+  const modules = {
+    toolbar: [['bold', 'italic', 'underline'], [{ align: [] }], [{ list: 'ordered' }, { list: 'bullet' }], ['link']],
+    clipboard: {
+      matchVisual: false,
+    },
+  };
   return (
     <Form.Item name={formName} initialValue={initialValue} dependencies={dependencies} rules={rules}>
-      <ReactQuill ref={currentReactQuillRef} placeholder={placeholder} className="text-editor" />
+      <ReactQuill ref={currentReactQuillRef} placeholder={placeholder} className="text-editor" modules={modules} />
     </Form.Item>
   );
 }
