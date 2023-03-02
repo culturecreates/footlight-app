e29b9d30472a490a209192b8f8b461a4fd97671e
Abhishek P Anil <abhishek@culturecreates.com>
2023-03-02 16:55:56 +0530

    feat: modal calendar components

--- src/pages/Dashboard/AddEvent/AddEvent.jsx
+++ src/pages/Dashboard/AddEvent/AddEvent.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useRef } from 'react';
 import './addEvent.css';
-import { Form, Row, Col, Input, Popover, message, Button } from 'antd';
+import { Form, Row, Col, Input, Popover, message, Button, Modal, Calendar, Space } from 'antd';
 import {
   SyncOutlined,
   InfoCircleOutlined,
@@ -8,6 +8,8 @@ import {
   CalendarOutlined,
   ControlOutlined,
   ExclamationCircleOutlined,
+  MinusCircleOutlined,
+  PlusOutlined,
 } from '@ant-design/icons';
 import moment from 'moment';
 import { useAddEventMutation, useUpdateEventMutation } from '../../../services/events';
@@ -116,6 +118,7 @@ function AddEvent() {
     performer: false,
     supporter: false,
   });
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const reactQuillRefFr = useRef(null);
   const reactQuillRefEn = useRef(null);
@@ -902,9 +905,50 @@ function AddEvent() {
                           </div>
                         </Form.Item>
                         <Form.Item>
-                          <Button type="text" icon={<ControlOutlined />}>
+                          <Button type="text" icon={<ControlOutlined />} onClick={() => setIsModalOpen(true)}>
                             Customize
                           </Button>
+                          <Modal
+                            title="Basic Modal"
+                            open={isModalOpen}
+                            onOk={() => setIsModalOpen(false)}
+                            onCancel={() => setIsModalOpen(false)}>
+                            <div>
+                              <div>
+                                <Calendar fullscreen={false} />
+                              </div>
+                              <div>
+                                <Form.List name="users">
+                                  {(fields, { add, remove }) => (
+                                    <>
+                                      {fields.map(({ key, name, ...restField }) => (
+                                        <Space key={key} style={{ display: 'flex', marginBottom: 8 }} align="baseline">
+                                          <Form.Item
+                                            {...restField}
+                                            name={[name, 'first']}
+                                            rules={[{ required: true, message: 'Missing first name' }]}>
+                                            <Input placeholder="First Name" />
+                                          </Form.Item>
+                                          <Form.Item
+                                            {...restField}
+                                            name={[name, 'last']}
+                                            rules={[{ required: true, message: 'Missing last name' }]}>
+                                            <Input placeholder="Last Name" />
+                                          </Form.Item>
+                                          <MinusCircleOutlined onClick={() => remove(name)} />
+                                        </Space>
+                                      ))}
+                                      <Form.Item>
+                                        <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined />}>
+                                          Add field
+                                        </Button>
+                                      </Form.Item>
+                                    </>
+                                  )}
+                                </Form.List>
+                              </div>
+                            </div>
+                          </Modal>
                         </Form.Item>
                       </Col>
                     </Row>
