d17137144f68dc21db01ea75974d62af83607b94
Abhishek P Anil <abhishek@culturecreates.com>
2023-03-07 10:56:47 +0530

    feat: french version for calendar added

--- src/components/DatePicker/DatePicker.jsx
+++ src/components/DatePicker/DatePicker.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import './datePicker.css';
 import { DatePicker } from 'antd';
 import i18n from 'i18next';
+import frLocale from 'antd/es/date-picker/locale/fr_CA';
+import enLocale from 'antd/es/date-picker/locale/en_US';
 
 function DatePickerStyled(props) {
   return (
     <DatePicker
       format="DD/MM/YYYY"
       size="large"
+      locale={i18n?.language === 'en' ? enLocale : i18n?.language === 'fr' && frLocale}
       className="date-picker-wrapper"
       {...props}
       placeholder={i18n?.language === 'en' ? 'DD/MM/YYYY' : 'JJ/MM/AAAA'}
--- src/components/DateRangePicker/DateRangePicker.jsx
+++ src/components/DateRangePicker/DateRangePicker.jsx
@@ -3,6 +3,8 @@ import './dateRangePicker.css';
 import { DatePicker } from 'antd';
 import { RightOutlined } from '@ant-design/icons';
 import i18n from 'i18next';
+import frLocale from 'antd/es/date-picker/locale/fr_CA';
+import enLocale from 'antd/es/date-picker/locale/en_US';
 
 const { RangePicker } = DatePicker;
 function DateRangePicker(props) {
@@ -12,6 +14,7 @@ function DateRangePicker(props) {
       popupClassName="date-range-picker-calendar"
       size={'large'}
       format="DD/MM/YYYY"
+      locale={i18n?.language === 'en' ? enLocale : i18n?.language === 'fr' && frLocale}
       getPopupContainer={(trigger) => trigger.parentNode}
       placeholder={i18n?.language === 'en' ? ['MM/DD/YYYY', 'MM/DD/YYYY'] : ['JJ/MM/AAAA', 'JJ/MM/AAAA']}
       separator={<RightOutlined style={{ color: '#646D7B' }} />}
