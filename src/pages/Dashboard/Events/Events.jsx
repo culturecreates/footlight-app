7664420e60cc1dd11d1e486e6c6c4fe5baab843d
Abhishek P Anil <abhishek@culturecreates.com>
2023-03-09 17:37:42 +0530

    feat: added  filter date value as the value for range picker

--- src/pages/Dashboard/Events/Events.jsx
+++ src/pages/Dashboard/Events/Events.jsx
@@ -291,6 +291,7 @@ function Events() {
                     // getPopupContainer={(trigger) => trigger.parentNode}
                     content={
                       <DateRangePicker
+                        value={filter?.dates}
                         onChange={(dates) => {
                           setFilter({ ...filter, dates: dates });
                         }}
