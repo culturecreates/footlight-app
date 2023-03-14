fe295ebc54a27d5a373f9e9cae106e2a2cb9d0b1
Abhishek P Anil <abhishek@culturecreates.com>
2023-03-14 15:39:10 +0530

    feat: commented the calendar antd code and introduced date picker again

--- src/components/MultipleDatePicker/MultipleDatePicker.jsx
+++ src/components/MultipleDatePicker/MultipleDatePicker.jsx
@@ -1,12 +1,100 @@
-import { Calendar, Col, Radio, Row, Select, Typography } from 'antd';
 import React, { useState } from 'react';
+import { DatePicker } from 'antd';
 import moment from 'moment';
+import './multipleDatePicker.css';
 const MultipleDatePicker = () => {
+  //   const [selectedDate, setSelectedDate] = useState([]);
+  //   const onPanelChange = (value, mode) => {
+  //     console.log(value.format('YYYY-MM-DD'), mode);
+  //   };
+  //   const onValueChange = (date) => {
+  //     const newDate = moment(date).startOf('day').valueOf();
+  //     if (selectedDate.includes(newDate)) {
+  //       setSelectedDate([...selectedDate.filter((item) => item !== newDate)]);
+  //     } else {
+  //       setSelectedDate([...selectedDate, newDate]);
+  //     }
+  //   };
+  //   const dateRender = (currentDate) => {
+  //     const isSelected = selectedDate.includes(moment(currentDate).startOf('day').valueOf());
+  //     let selectStyle = isSelected
+  //       ? {
+  //           position: 'relative',
+  //           zIndex: 2,
+  //           display: 'inlineBlock',
+  //           width: '24px',
+  //           height: '22px',
+  //           lineHeight: '22px',
+  //           backgroundColor: '#1890ff',
+  //           color: '#fff',
+  //           margin: 'auto',
+  //           borderRadius: '2px',
+  //           transition: 'background 0.3s, border 0.3s',
+  //         }
+  //       : {};
+  //     return <div style={selectStyle}> {currentDate.date()} </div>;
+  //   };
+  //   return (
+  //     <div className="site-calendar-customize-header-wrapper">
+  //       <Calendar
+  //         fullscreen={false}
+  //         headerRender={({ value, onTypeChange }) => {
+  //           const start = 0;
+  //           const end = 12;
+  //           const monthOptions = [];
+  //           const current = value.clone();
+  //           const localeData = value.localeData();
+  //           const months = [];
+  //           for (let i = 0; i < 12; i++) {
+  //             current.month(i);
+  //             months.push(localeData.monthsShort(current));
+  //           }
+  //           for (let i = start; i < end; i++) {
+  //             monthOptions.push(
+  //               <Select.Option key={i} value={i} className="month-item">
+  //                 {months[i]}
+  //               </Select.Option>,
+  //             );
+  //           }
+  //           const year = value.year();
+  //           const month = value.month();
+  //           const options = [];
+  //           for (let i = year - 10; i < year + 100; i += 1) {
+  //             options.push(
+  //               <Select.Option key={i} value={i} className="year-item">
+  //                 {i}
+  //               </Select.Option>,
+  //             );
+  //           }
+  //           return (
+  //             <div
+  //               style={{
+  //                 padding: 8,
+  //               }}>
+  //               <Row gutter={8}>
+  //                 <Col>{'<'}</Col>
+  //                 <Col>{'<<'}</Col>
+  //                 <Col>
+  //                   <div onClick={onTypeChange} value="year">{month}</div>
+  //                 </Col>
+  //                 <Col>{year}</Col>
+  //                 <Col>{'>'}</Col>
+  //                 <Col>{'>>'}</Col>
+  //               </Row>
+  //             </div>
+  //           );
+  //         }}
+  //         onPanelChange={onPanelChange}
+  //         dateFullCellRender={dateRender}
+  //         onSelect={onValueChange}
+  //       />
+  //     </div>
+  //   );
+
+  // 选中的日期 timestamp[]
   const [selectedDate, setSelectedDate] = useState([]);
 
-  const onPanelChange = (value, mode) => {
-    console.log(value.format('YYYY-MM-DD'), mode);
-  };
+  // 日期发生变化时 重复去重 没有则添加
   const onValueChange = (date) => {
     const newDate = moment(date).startOf('day').valueOf();
     if (selectedDate.includes(newDate)) {
@@ -16,6 +104,7 @@ const MultipleDatePicker = () => {
     }
   };
 
+  // 渲染选中日期外观
   const dateRender = (currentDate) => {
     const isSelected = selectedDate.includes(moment(currentDate).startOf('day').valueOf());
     let selectStyle = isSelected
@@ -35,85 +124,13 @@ const MultipleDatePicker = () => {
       : {};
     return <div style={selectStyle}> {currentDate.date()} </div>;
   };
+
   return (
-    <div className="site-calendar-customize-header-wrapper">
-      <Calendar
-        fullscreen={false}
-        headerRender={({ value, type, onChange, onTypeChange }) => {
-          const start = 0;
-          const end = 12;
-          const monthOptions = [];
-          const current = value.clone();
-          const localeData = value.localeData();
-          const months = [];
-          for (let i = 0; i < 12; i++) {
-            current.month(i);
-            months.push(localeData.monthsShort(current));
-          }
-          for (let i = start; i < end; i++) {
-            monthOptions.push(
-              <Select.Option key={i} value={i} className="month-item">
-                {months[i]}
-              </Select.Option>,
-            );
-          }
-          const year = value.year();
-          const month = value.month();
-          const options = [];
-          for (let i = year - 10; i < year + 10; i += 1) {
-            options.push(
-              <Select.Option key={i} value={i} className="year-item">
-                {i}
-              </Select.Option>,
-            );
-          }
-          return (
-            <div
-              style={{
-                padding: 8,
-              }}>
-              <Typography.Title level={4}>Custom header</Typography.Title>
-              <Row gutter={8}>
-                <Col>
-                  <Radio.Group size="small" onChange={(e) => onTypeChange(e.target.value)} value={type}>
-                    <Radio.Button value="month">Month</Radio.Button>
-                    <Radio.Button value="year">Year</Radio.Button>
-                  </Radio.Group>
-                </Col>
-                <Col>
-                  <Select
-                    size="small"
-                    dropdownMatchSelectWidth={false}
-                    className="my-year-select"
-                    value={year}
-                    onChange={(newYear) => {
-                      const now = value.clone().year(newYear);
-                      onChange(now);
-                    }}>
-                    {options}
-                  </Select>
-                </Col>
-                <Col>
-                  <Select
-                    size="small"
-                    dropdownMatchSelectWidth={false}
-                    value={month}
-                    onChange={(newMonth) => {
-                      const now = value.clone().month(newMonth);
-                      onChange(now);
-                    }}>
-                    {monthOptions}
-                  </Select>
-                </Col>
-              </Row>
-            </div>
-          );
-        }}
-        onPanelChange={onPanelChange}
-        dateFullCellRender={dateRender}
-        onChange={onValueChange}
-      />
-    </div>
+    <>
+      <div>
+        <DatePicker open dateRender={dateRender} onChange={onValueChange} showToday={false} value={''} />
+      </div>
+    </>
   );
 };
 export default MultipleDatePicker;
