a11effd50698a5e6bfcb75a09210ca9245590ab3
Abhishek P Anil <abhishek@culturecreates.com>
2023-03-20 15:52:53 +0530

    feat: added the get api integration for invitation id

--- src/pages/CreateAccount/CreateAccount.jsx
+++ src/pages/CreateAccount/CreateAccount.jsx
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Form, notification } from 'antd';
 import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
 import { useTranslation } from 'react-i18next';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import './createAccount.css';
 import LoginButton from '../../components/Button/Auth';
 import { PathName } from '../../constants/pathName';
 import { useResetPasswordMutation } from '../../services/users';
 import Auth from '../../layout/Auth';
 import PasswordInput from '../../components/Input/Password';
+import { useGetInviteDetailsQuery } from '../../services/invite';
 
 function CreateAccount() {
   const { t } = useTranslation();
   const [form] = Form.useForm();
   const navigate = useNavigate();
+  const timestampRef = useRef(Date.now()).current;
+  let [searchParams] = useSearchParams();
+
   const [resetPassword] = useResetPasswordMutation();
+
+  let invitationId = searchParams.get('invitationId');
+
+  const {
+    currentData: inviteUserData,
+    isLoading: inviteUserLoading,
+    error: inviteUserError,
+  } = useGetInviteDetailsQuery({
+    id: invitationId,
+    sessionId: timestampRef,
+  });
 
   const onFinish = (values) => {
     resetPassword({
@@ -32,75 +47,94 @@ function CreateAccount() {
     });
   };
 
-  return (
-    <Auth>
-      <div className="create-account-page-wrapper">
-        <h3 className="create-account-heading">{t('createAccount.createYourAccount')}</h3>
-        <Form
-          name="createAccountForm"
-          className="create-account-form"
-          layout="vertical"
-          autoComplete="off"
-          requiredMark={false}
-          scrollToFirstError={true}
-          validateTrigger={'onBlur'}
-          form={form}
-          onFinish={onFinish}>
-          <Form.Item className="create-account-form-item" label={t('createAccount.firstName')} labelAlign="left">
-            <p className="create-account-read-only-content">fsofjsj</p>
-          </Form.Item>
-          <Form.Item className="create-account-form-item" label={t('createAccount.secondName')} labelAlign="left">
-            <p className="create-account-read-only-content">fsofjsj</p>
-          </Form.Item>
-          <Form.Item className="create-account-form-item" label={t('createAccount.email')} labelAlign="left">
-            <p className="create-account-read-only-content">fsofjsj</p>
-          </Form.Item>
-          <Form.Item
-            className="create-account-form-item"
-            name="newPassword"
-            label={t('createAccount.newPassword')}
-            labelAlign="left"
-            rules={[
-              {
-                required: true,
-                message: t('resetPassword.validations.emptyPassword'),
-              },
-            ]}>
-            <PasswordInput
-              placeholder={t('resetPassword.passwordPlaceHolder')}
-              iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
-            />
-          </Form.Item>
-          <Form.Item
-            className="create-account-form-item"
-            name="confirmNewPassword"
-            label={t('createAccount.confirmNewPassword')}
-            labelAlign="left"
-            rules={[
-              {
-                required: true,
-                message: t('resetPassword.validations.emptyPassword'),
-              },
-              ({ getFieldValue }) => ({
-                validator(_, value) {
-                  if (!value || getFieldValue('newPassword') === value) {
-                    return Promise.resolve();
-                  } else return Promise.reject(new Error(t('resetPassword.validations.passwordMatch')));
-                },
-              }),
-            ]}>
-            <PasswordInput
-              placeholder={t('createAccount.passwordPlaceHolder')}
-              iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
-            />
-          </Form.Item>
+  if (inviteUserError) navigate('/');
 
-          <Form.Item className="create-account-reset-my-password">
-            <LoginButton label={t('createAccount.acceptInvitation')} htmlType="htmlType" />
-          </Form.Item>
-        </Form>
-      </div>
-    </Auth>
+  return (
+    !inviteUserLoading && (
+      <Auth>
+        <div className="create-account-page-wrapper">
+          <h3 className="create-account-heading">{t('createAccount.createYourAccount')}</h3>
+          <Form
+            name="createAccountForm"
+            className="create-account-form"
+            layout="vertical"
+            autoComplete="off"
+            requiredMark={false}
+            scrollToFirstError={true}
+            validateTrigger={'onBlur'}
+            form={form}
+            onFinish={onFinish}>
+            <Form.Item
+              className="create-account-form-item"
+              label={t('createAccount.firstName')}
+              labelAlign="left"
+              name="firstName"
+              initialValue={inviteUserData?.firstName}>
+              <p className="create-account-read-only-content">{inviteUserData?.firstName}</p>
+            </Form.Item>
+            <Form.Item
+              className="create-account-form-item"
+              label={t('createAccount.lastName')}
+              labelAlign="left"
+              name="lastName"
+              initialValue={inviteUserData?.secondName}>
+              <p className="create-account-read-only-content">{inviteUserData?.lastName}</p>
+            </Form.Item>
+            <Form.Item
+              className="create-account-form-item"
+              label={t('createAccount.email')}
+              labelAlign="left"
+              name="email"
+              initialValue={inviteUserData?.email}>
+              <p className="create-account-read-only-content">{inviteUserData?.email}</p>
+            </Form.Item>
+            <Form.Item
+              className="create-account-form-item"
+              name="newPassword"
+              label={t('createAccount.newPassword')}
+              labelAlign="left"
+              rules={[
+                {
+                  required: true,
+                  message: t('resetPassword.validations.emptyPassword'),
+                },
+              ]}>
+              <PasswordInput
+                placeholder={t('resetPassword.passwordPlaceHolder')}
+                iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+              />
+            </Form.Item>
+            <Form.Item
+              className="create-account-form-item"
+              name="confirmNewPassword"
+              label={t('createAccount.confirmNewPassword')}
+              labelAlign="left"
+              rules={[
+                {
+                  required: true,
+                  message: t('resetPassword.validations.emptyPassword'),
+                },
+                ({ getFieldValue }) => ({
+                  validator(_, value) {
+                    if (!value || getFieldValue('newPassword') === value) {
+                      return Promise.resolve();
+                    } else return Promise.reject(new Error(t('resetPassword.validations.passwordMatch')));
+                  },
+                }),
+              ]}>
+              <PasswordInput
+                placeholder={t('createAccount.passwordPlaceHolder')}
+                iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+              />
+            </Form.Item>
+
+            <Form.Item className="create-account-reset-my-password">
+              <LoginButton label={t('createAccount.acceptInvitation')} htmlType="htmlType" />
+            </Form.Item>
+          </Form>
+        </div>
+      </Auth>
+    )
   );
 }
 
--- src/services/invite.js
+++ src/services/invite.js
@@ -11,6 +11,7 @@ export const inviteApi = createApi({
           method: 'GET',
         };
       },
+      transformResponse: (response) => response.data,
     }),
 
     acceptInvite: builder.mutation({
