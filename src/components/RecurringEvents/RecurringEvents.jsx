02bd69e25e8dd331c081ef77446024659b488771
Abhishek P Anil <abhishek@culturecreates.com>
2023-03-27 18:53:14 +0530

    fix: added subevents count function

--- src/components/RecurringEvents/RecurringEvents.jsx
+++ src/components/RecurringEvents/RecurringEvents.jsx
@@ -274,6 +274,18 @@ const RecurringEvents = function ({
       daysOfWeek: selectedWeekDaysArray,
     });
   };
+  // const subEventsCountHandler = (events) => {
+  //   var numberOfSubEvents = 0;
+  //   events?.map((event) => {
+  //     if (!event?.isDeleted) {
+  //       if (event?.time?.length > 0) numberOfSubEvents = numberOfSubEvents + event?.time?.length;
+  //       else if (event?.time?.length === 0 || !event?.time) {
+  //         numberOfSubEvents = numberOfSubEvents + 1;
+  //       }
+  //     }
+  //   });
+  //   return numberOfSubEvents;
+  // };
   return (
     <div className="recurring-events-wrapper">
       {/* <Form.Item
