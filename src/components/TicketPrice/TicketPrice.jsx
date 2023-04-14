79afd58f8350bee1d398647e395210487a0b89a0
Abhishek P Anil <abhishek@culturecreates.com>
2023-04-14 15:53:47 +0530

    fix: removed the errors on console for descendant button and table error

--- src/components/TicketPrice/TicketPrice.jsx
+++ src/components/TicketPrice/TicketPrice.jsx
@@ -61,27 +61,34 @@ function TicketPrice(props) {
             </td>
           </tr>
         ))}
-        <Form.Item>
-          <Button
-            type="text"
-            size="small"
-            style={{ color: '#1B3DE6', display: 'flex', gap: '8px' }}
-            onClick={() => add()}
-            icon={
+        <tr>
+          <td>
+            <Form.Item>
               <Button
-                shape="default"
+                type="text"
                 size="small"
-                style={{
-                  backgroundColor: '#EFF2FF',
-                  borderRadius: '4px',
-                  border: '0px',
-                }}>
-                <PlusOutlined style={{ color: '#1B3DE6' }} />
+                style={{ color: '#1B3DE6', display: 'flex', gap: '8px' }}
+                onClick={() => add()}
+                icon={
+                  <div
+                    style={{
+                      backgroundColor: '#EFF2FF',
+                      borderRadius: '4px',
+                      border: '0px',
+                      height: '24px',
+                      width: '24px',
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                    }}>
+                    <PlusOutlined style={{ color: '#1B3DE6' }} />
+                  </div>
+                }>
+                {t('dashboard.events.addEditEvent.tickets.anotherTicketType')}
               </Button>
-            }>
-            {t('dashboard.events.addEditEvent.tickets.anotherTicketType')}
-          </Button>
-        </Form.Item>
+            </Form.Item>
+          </td>
+        </tr>
       </tbody>
     </table>
   );
