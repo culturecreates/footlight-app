40fb6b54652ad4857ba0e43e167c9578e1f7b33b
Abhishek P Anil <abhishek@culturecreates.com>
2023-06-02 16:06:26 +0530

    feat: added width for modal and date action button

--- src/components/Modal/QuickSelect/QuickSelect.jsx
+++ src/components/Modal/QuickSelect/QuickSelect.jsx
@@ -13,6 +13,7 @@ function QuickSelect(props) {
 
   return (
     <CustomModal
+      width={500}
       open={open}
       onCancel={() => setOpen(false)}
       title={<span className="quick-select-modal-title">{t('dashboard.events.addEditEvent.quickCreate.title')}</span>}
@@ -24,16 +25,18 @@ function QuickSelect(props) {
           </span>
         </Col>
         <Col span={24}>
-          <Row align={'middle'} justify={'center'}>
-            <Col span={8}>
+          <Row align={'middle'} justify={'center'} gutter={[36, 0]}>
+            <Col span={12}>
               <DateAction
+                style={{ width: '200px', height: '125px' }}
                 iconrender={<UserOutlined style={{ fontSize: '12px', color: '#9196A3' }} />}
                 label={t('dashboard.events.addEditEvent.quickCreate.person')}
                 disabled
               />
             </Col>
-            <Col span={8}>
+            <Col span={12}>
               <DateAction
+                style={{ width: '200px', height: '125px' }}
                 iconrender={
                   <Icon
                     component={Organizations}
