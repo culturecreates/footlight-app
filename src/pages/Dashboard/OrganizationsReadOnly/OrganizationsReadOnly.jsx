95f6f41b3e3ea8d828afd2ed87c3260a2e236470
Abhishek P Anil <abhishek@culturecreates.com>
2023-07-12 15:39:46 +0530

    feat: added image for organization

--- src/pages/Dashboard/OrganizationsReadOnly/OrganizationsReadOnly.jsx
+++ src/pages/Dashboard/OrganizationsReadOnly/OrganizationsReadOnly.jsx
@@ -347,7 +347,18 @@ function OrganizationsReadOnly() {
               )}
             </Row>
           </Col>
-          <></>
+          <Col>
+            <div style={{ marginTop: '-35%' }}>
+              <img
+                src={organizationData?.image?.original?.uri}
+                alt="avatar"
+                style={{
+                  width: '151px',
+                  objectFit: 'cover',
+                }}
+              />
+            </div>
+          </Col>
         </Card>
       </Row>
     )
