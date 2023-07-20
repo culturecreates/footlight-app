06a1b3063b3b1ebe8ce145539cc0f41aadae56ff
Abhishek P Anil <abhishek@culturecreates.com>
2023-07-20 15:16:49 +0530

    feat: artsdata link added for read only page

---
+++ src/components/ArtsDataInfo/ArtsDataInfo.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './artsDataInfo.css';
+import { Badge, Col, Row } from 'antd';
+import { LinkOutlined } from '@ant-design/icons';
+
+function ArtsDataInfo(props) {
+  const { artsDataLink, name, disambiguatingDescription } = props;
+  return (
+    <Row className="arts-data-info" align={'middle'} gutter={[4, 0]}>
+      <Col className="arts-data-title" onClick={() => window.open(`${artsDataLink}`, '_blank', 'noopener,noreferrer')}>
+        <span style={{ textDecoration: 'underline' }}>Artsdata</span>
+      </Col>
+      <Col className="arts-data-name">
+        <span>{name}</span>
+      </Col>
+      <Col>
+        <span>
+          <Badge color="#1B3DE6" />
+        </span>
+      </Col>
+      <Col className="arts-data-description">{disambiguatingDescription}</Col>
+      <Col onClick={() => window.open(`${artsDataLink}`, '_blank', 'noopener,noreferrer')}>
+        <span>
+          <LinkOutlined />
+        </span>
+      </Col>
+    </Row>
+  );
+}
+
+export default ArtsDataInfo;
