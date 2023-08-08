b1a3e45007766df462bdf2e0725de6fe770917a3
Abhishek P Anil <abhishek@culturecreates.com>
2023-08-08 10:35:24 +0530

    feat: added the basic cropping modal and package

--- src/components/ImageUpload/ImageUpload.jsx
+++ src/components/ImageUpload/ImageUpload.jsx
@@ -4,6 +4,7 @@ import { message, Upload, Form } from 'antd';
 import { LoadingOutlined, DownloadOutlined, DeleteOutlined } from '@ant-design/icons';
 import Outlined from '../Button/Outlined';
 import { useTranslation } from 'react-i18next';
+import ImgCrop from 'antd-img-crop';
 
 function ImageUpload(props) {
   const { t } = useTranslation();
@@ -19,9 +20,21 @@ function ImageUpload(props) {
   const getBase64 = (img, callback) => {
     const reader = new FileReader();
     reader.addEventListener('load', () => callback(reader.result));
+
     reader.readAsDataURL(img);
+    reader.addEventListener('load', (event) => {
+      const _loadedImageUrl = event.target.result;
+      const image = document.createElement('img');
+      image.src = _loadedImageUrl;
+      image.addEventListener('load', () => {
+        const { width, height } = image;
+        // set image width and height to your state here
+        console.log(width, height);
+      });
+    });
   };
   const beforeUpload = (file) => {
+    console.log(file);
     const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
     if (!isJpgOrPng) {
       message.error(t('dashboard.events.addEditEvent.otherInformation.image.subHeading'));
@@ -68,50 +81,57 @@ function ImageUpload(props) {
       </span>
     </div>
   );
-
+  const onCropAreaChange = (croppedArea, croppedAreaPixel) => {
+    console.log(croppedArea, croppedAreaPixel);
+  };
   return (
     <Form.Item name="dragger" valuePropName="fileList" getValueFromEvent={normFile}>
-      <Upload.Dragger
-        name="eventImage"
-        accept='.png, .jpg, .jpeg"'
-        className="upload-wrapper"
-        multiple={false}
-        customRequest={customRequest}
-        disabled={props?.imageReadOnly}
-        maxCount={1}
-        onRemove={onRemove}
-        defaultFileList={
-          props?.imageUrl && [
-            {
-              uid: '1',
-              name: 'Event image',
-              status: 'done',
-              url: props.imageUrl,
-            },
-          ]
-        }
-        listType="picture"
-        beforeUpload={beforeUpload}
-        showUploadList={{
-          showDownloadIcon: props?.imageReadOnly ? true : false,
-          downloadIcon: <DownloadOutlined style={{ color: '#1B3DE6' }} />,
-          showRemoveIcon: imageUrl ? true : false,
-          removeIcon: <DeleteOutlined style={{ color: '#1B3DE6', fontWeight: '600', fontSize: '16px' }} />,
-        }}
-        onChange={handleChange}>
-        {imageUrl && props?.preview ? (
-          <img
-            src={imageUrl}
-            alt="avatar"
-            style={{
-              width: '423px',
-              objectFit: 'cover',
-            }}
-          />
-        ) : (
-          uploadButton
-        )}
-      </Upload.Dragger>
+      <ImgCrop
+        cropperProps={{
+          onCropAreaChange: onCropAreaChange,
+        }}>
+        <Upload.Dragger
+          name="eventImage"
+          accept='.png, .jpg, .jpeg"'
+          className="upload-wrapper"
+          multiple={false}
+          customRequest={customRequest}
+          disabled={props?.imageReadOnly}
+          maxCount={1}
+          onRemove={onRemove}
+          defaultFileList={
+            props?.imageUrl && [
+              {
+                uid: '1',
+                name: 'Event image',
+                status: 'done',
+                url: props.imageUrl,
+              },
+            ]
+          }
+          listType="picture"
+          beforeUpload={beforeUpload}
+          showUploadList={{
+            showDownloadIcon: props?.imageReadOnly ? true : false,
+            downloadIcon: <DownloadOutlined style={{ color: '#1B3DE6' }} />,
+            showRemoveIcon: imageUrl ? true : false,
+            removeIcon: <DeleteOutlined style={{ color: '#1B3DE6', fontWeight: '600', fontSize: '16px' }} />,
+          }}
+          onChange={handleChange}>
+          {imageUrl && props?.preview ? (
+            <img
+              src={imageUrl}
+              alt="avatar"
+              style={{
+                width: '423px',
+                objectFit: 'cover',
+              }}
+            />
+          ) : (
+            uploadButton
+          )}
+        </Upload.Dragger>
+      </ImgCrop>
     </Form.Item>
   );
 }
