16e302e37097b284b73d03a50a34b8c3b3ab8d61
syam babu <syam@dexlock.com>
2023-08-16 17:37:05 +0530

    Fix:rewrote layout using antd columns and rows

--- src/layout/CreateNewEntity/NewEntityLayout.jsx
+++ src/layout/CreateNewEntity/NewEntityLayout.jsx
@@ -1,26 +1,28 @@
-import { Button } from 'antd';
+import { Button, Row, Col } from 'antd';
 import React from 'react';
 import './createNew.css';
 
 const NewEntityLayout = ({ children, heading, text }) => {
   return (
-    <section className="create-new-entity-page">
-      <header>
+    <Row className="create-new-entity-page">
+      <Col span={24}>
         <div className="button-container">
-          <Button>back to previous screen</Button>
+          <Button type="link">back to previous screen</Button>
         </div>
         <h1 className="heading">{`New ${heading}`}</h1>
-      </header>
+      </Col>
 
-      <div className="content">
-        <h2 className="sub-heading">Search for another instance</h2>
-        <p>{text}</p>
-        <div className="search">
+      <Row className="content">
+        <Col span={24}>
+          <h2 className="sub-heading">Search for another instance</h2>
+          <p>{text}</p>
+        </Col>
+        <Col span={24} className="search">
           <p>{heading}</p>
           {children}
-        </div>
-      </div>
-    </section>
+        </Col>
+      </Row>
+    </Row>
   );
 };
 
