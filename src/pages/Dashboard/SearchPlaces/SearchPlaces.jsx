7f5d295bfa08d9c6ef40f58428351a3007d7e9d5
syam babu <syam@dexlock.com>
2023-08-23 18:26:41 +0530

    feat: api integration completed

--- src/pages/Dashboard/SearchPlaces/SearchPlaces.jsx
+++ src/pages/Dashboard/SearchPlaces/SearchPlaces.jsx
@@ -1,5 +1,5 @@
 import { Popover } from 'antd';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { useNavigate, useOutletContext, useParams } from 'react-router-dom';
@@ -14,6 +14,8 @@ import { artsDataLinkChecker } from '../../../utils/artsDataLinkChecker';
 import { contentLanguageBilingual } from '../../../utils/bilingual';
 import { EnvironmentOutlined } from '@ant-design/icons';
 import './searchPlaces.css';
+import { entitiesClass } from '../../../constants/entitiesClass';
+import { useGetEntitiesQuery, useLazyGetEntitiesQuery } from '../../../services/entities';
 
 function SearchPlaces() {
   const { t } = useTranslation();
@@ -22,7 +24,7 @@ function SearchPlaces() {
   const navigate = useNavigate();
 
   const { calendarId } = useParams();
-  //   const timestampRef = useRef(Date.now()).current;
+  const timestampRef = useRef(Date.now()).current;
   const calendarContentLanguage = currentCalendarData?.contentLanguage;
 
   const [isPopoverOpen, setIsPopoverOpen] = useState(false);
@@ -30,101 +32,122 @@ function SearchPlaces() {
   const [selectedPlaces, setSelectedPlaces] = useState([]);
   const [quickCreateKeyword, setQuickCreateKeyword] = useState('');
 
+  const [getEntities] = useLazyGetEntitiesQuery({ sessionId: timestampRef });
+
+  let query = new URLSearchParams();
+  query.append('classes', entitiesClass.place);
+  const { currentData: initialEntities, isLoading: initialPlacesLoading } = useGetEntitiesQuery({
+    calendarId,
+    searchKey: '',
+    classes: decodeURIComponent(query.toString()),
+    sessionId: timestampRef,
+  });
+
   useEffect(() => {
-    setPlacesList([]);
-  }, []);
+    if (initialEntities && currentCalendarData) {
+      setPlacesList(initialEntities);
+    }
+  }, [initialPlacesLoading]);
+
+  // handlers
 
   const searchHandler = (value) => {
-    console.log(value);
+    let query = new URLSearchParams();
+    query.append('classes', entitiesClass.place);
+    getEntities({ searchKey: value, classes: decodeURIComponent(query.toString()), calendarId })
+      .unwrap()
+      .then((response) => {
+        setPlacesList(response);
+      })
+      .catch((error) => console.log(error));
   };
 
   return (
-    <NewEntityLayout
-      heading={t('dashboard.places.createNew.search.title')}
-      entityName={t('dashboard.places.createNew.search.searchbarHeader')}
-      text={t('dashboard.places.createNew.search.text')}>
-      <div className="search-bar-places">
-        <Popover
-          open={isPopoverOpen}
-          arrow={false}
-          overlayClassName="event-popover"
-          placement="bottom"
-          onOpenChange={(open) => {
-            setIsPopoverOpen(open);
-            searchHandler(quickCreateKeyword);
-          }}
-          autoAdjustOverflow={false}
-          getPopupContainer={(trigger) => trigger.parentNode}
-          trigger={['click']}
-          content={
-            <div>
-              <div className="search-scrollable-content">
-                {placesList?.length > 0 ? (
-                  placesList?.map((organizer, index) => (
-                    <div
-                      key={index}
-                      className="search-popover-options"
-                      onClick={() => {
-                        setSelectedPlaces([...selectedPlaces, organizer]);
-                        setIsPopoverOpen(false);
-                      }}>
-                      <EntityCard
-                        title={contentLanguageBilingual({
-                          en: organizer?.name?.en,
-                          fr: organizer?.name?.fr,
-                          interfaceLanguage: user?.interfaceLanguage?.toLowerCase(),
-                          calendarContentLanguage: calendarContentLanguage,
-                        })}
-                        description={contentLanguageBilingual({
-                          en: organizer?.disambiguatingDescription?.en,
-                          fr: organizer?.disambiguatingDescription?.fr,
-                          interfaceLanguage: user?.interfaceLanguage?.toLowerCase(),
-                          calendarContentLanguage: calendarContentLanguage,
-                        })}
-                        artsDataLink={artsDataLinkChecker(organizer?.uri)}
-                        Logo={<EnvironmentOutlined style={{ color: '#607EFC', fontSize: '18px' }} />}
-                        linkText={t('dashboard.places.createNew.search.linkText')}
-                        onClick={() =>
-                          navigate(
-                            `${PathName.Dashboard}/${calendarId}${PathName.Places}${PathName.Places}${PathName.Search}`,
-                          )
-                        }
-                      />
-                    </div>
-                  ))
-                ) : (
-                  <NoContent />
+    !initialPlacesLoading && (
+      <NewEntityLayout
+        heading={t('dashboard.places.createNew.search.title')}
+        entityName={t('dashboard.places.createNew.search.searchbarHeader')}
+        text={t('dashboard.places.createNew.search.text')}>
+        <div className="search-bar-places">
+          <Popover
+            open={isPopoverOpen}
+            arrow={false}
+            overlayClassName="event-popover"
+            placement="bottom"
+            onOpenChange={(open) => {
+              setIsPopoverOpen(open);
+              searchHandler(quickCreateKeyword);
+            }}
+            autoAdjustOverflow={false}
+            getPopupContainer={(trigger) => trigger.parentNode}
+            trigger={['click']}
+            content={
+              <div>
+                <div className="search-scrollable-content">
+                  {placesList?.length > 0 ? (
+                    placesList?.map((place, index) => (
+                      <div
+                        key={index}
+                        className="search-popover-options"
+                        onClick={() => {
+                          setSelectedPlaces([...selectedPlaces, place]);
+                          setIsPopoverOpen(false);
+                        }}>
+                        <EntityCard
+                          title={contentLanguageBilingual({
+                            en: place?.name?.en,
+                            fr: place?.name?.fr,
+                            interfaceLanguage: user?.interfaceLanguage?.toLowerCase(),
+                            calendarContentLanguage: calendarContentLanguage,
+                          })}
+                          description={contentLanguageBilingual({
+                            en: place?.disambiguatingDescription?.en,
+                            fr: place?.disambiguatingDescription?.fr,
+                            interfaceLanguage: user?.interfaceLanguage?.toLowerCase(),
+                            calendarContentLanguage: calendarContentLanguage,
+                          })}
+                          artsDataLink={artsDataLinkChecker(place?.uri)}
+                          Logo={<EnvironmentOutlined style={{ color: '#607EFC', fontSize: '18px' }} />}
+                          linkText={t('dashboard.places.createNew.search.linkText')}
+                          onClick={() =>
+                            navigate(`${PathName.Dashboard}/${calendarId}${PathName.Places}${PathName.Search}`)
+                          }
+                        />
+                      </div>
+                    ))
+                  ) : (
+                    <NoContent />
+                  )}
+                </div>
+                {quickCreateKeyword?.length > 0 && (
+                  <CreateEntityButton
+                    quickCreateKeyword={quickCreateKeyword}
+                    onClick={() => {
+                      navigate(`${PathName.Dashboard}/${calendarId}${PathName.Places}${PathName.Search}`, {
+                        name: quickCreateKeyword,
+                      });
+                    }}
+                  />
                 )}
               </div>
-              {quickCreateKeyword?.length > 0 && (
-                <CreateEntityButton
-                  quickCreateKeyword={quickCreateKeyword}
-                  onClick={() => {
-                    navigate(
-                      `${PathName.Dashboard}/${calendarId}${PathName.Places}${PathName.Places}${PathName.Search}`,
-                      { name: quickCreateKeyword },
-                    );
-                  }}
-                />
-              )}
-            </div>
-          }>
-          <EventsSearch
-            style={{ borderRadius: '4px' }}
-            placeholder="Search places"
-            onClick={(e) => {
-              setQuickCreateKeyword(e.target.value);
-              setIsPopoverOpen(true);
-            }}
-            onChange={(e) => {
-              setQuickCreateKeyword(e.target.value);
-              searchHandler(e.target.value);
-              setIsPopoverOpen(true);
-            }}
-          />
-        </Popover>
-      </div>
-    </NewEntityLayout>
+            }>
+            <EventsSearch
+              style={{ borderRadius: '4px' }}
+              placeholder="Search places"
+              onClick={(e) => {
+                setQuickCreateKeyword(e.target.value);
+                setIsPopoverOpen(true);
+              }}
+              onChange={(e) => {
+                setQuickCreateKeyword(e.target.value);
+                searchHandler(e.target.value);
+                setIsPopoverOpen(true);
+              }}
+            />
+          </Popover>
+        </div>
+      </NewEntityLayout>
+    )
   );
 }
 
