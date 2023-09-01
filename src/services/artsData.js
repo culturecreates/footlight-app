524a1145b56e8bbe2eaf15e75db6ff8057122a64
syam babu <syam@dexlock.com>
2023-09-01 17:34:26 +0530

    fix: minor css fix. removed unwanted comments

--- src/services/artsData.js
+++ src/services/artsData.js
@@ -1,6 +1,5 @@
 const baseUrl = process.env.REACT_APP_ARTS_DATA_URI;
 
-// Helper function to make a GET request using fetch
 export async function fetchData(url) {
   const response = await fetch(url);
   if (!response.ok) {
@@ -9,14 +8,12 @@ export async function fetchData(url) {
   return response.json();
 }
 
-// Function to get Arts Data Entities
 export async function getArtsDataEntities({ searchKeyword, entityType }) {
   const query = `recon?query=${searchKeyword}&type=schema:${entityType}`;
   const url = `${baseUrl}/${query}`;
   return fetchData(url);
 }
 
-// Function to load Arts Data Entity
 export async function loadArtsDataEntity({ entityId }) {
   const query = `query?adid=${entityId}&format=json&frame=ranked_org_person_footlight&sparql=ranked_org_person_footlight`;
   const url = `${baseUrl}/${query}`;
