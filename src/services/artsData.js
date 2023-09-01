18c8d16ae10d893ad8c7e49331242311f99e9c7f
syam babu <syam@dexlock.com>
2023-09-01 16:12:31 +0530

    feat: added load data query

--- src/services/artsData.js
+++ src/services/artsData.js
@@ -5,9 +5,13 @@ export const artsDataApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_ARTS_DATA_URI }),
   endpoints: (builder) => ({
     getArtsDataEntities: builder.query({
-      query: ({ searchKeyword, entityType }) => `/recon?query=${searchKeyword}&type=schema:${entityType}`,
+      query: ({ searchKeyword, entityType }) => `recon?query=${searchKeyword}&type=schema:${entityType}`,
+    }),
+    loadArtsDataEntity: builder.query({
+      query: ({ entityId }) =>
+        `query?adid=${entityId}&format=json&frame=ranked_org_person_footlight&sparql=ranked_org_person_footlight`,
     }),
   }),
 });
 
-export const { useGetArtsDataEntitiesQuery, useLazyGetArtsDataEntitiesQuery } = artsDataApi;
+export const { useLazyLoadArtsDataEntityQuery, useLazyGetArtsDataEntitiesQuery } = artsDataApi;
