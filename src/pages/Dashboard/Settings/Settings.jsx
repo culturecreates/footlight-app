457a85b8de046b1d498d040dd5d2bfee9b5113ce
syam babu <syam@dexlock.com>
2023-09-12 14:08:30 +0530

    feat: settings layout page

--- src/pages/Dashboard/Settings/Settings.jsx
+++ src/pages/Dashboard/Settings/Settings.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import { Col, Row, Tabs } from 'antd';
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import FeatureFlag from '../../../layout/FeatureFlag/FeatureFlag';
+import { featureFlags } from '../../../utils/featureFlags';
+import './settings.css';
+import UserManagement from './UserManagement/UserManagement';
 
 const Settings = () => {
-  return <div>Settings</div>;
+  const { t } = useTranslation();
+  const [tabKey, setTabKey] = useState('tab1');
+
+  const onTabChange = (key) => {
+    setTabKey(key);
+  };
+
+  const items = [
+    { label: t('dashboard.settings.tab1'), key: 'tab1', children: <UserManagement /> },
+    { label: t('dashboard.settings.tab2'), key: 'tab2', children: 'Coming soon' },
+  ];
+
+  return (
+    <FeatureFlag isFeatureEnabled={featureFlags.settingsScreenUsers}>
+      <Row className="settings-wrapper">
+        <Col span={24}>
+          <h4 className="settings-heading">{t('dashboard.settings.heading')}</h4>
+        </Col>
+        <Col span={24}>
+          <Tabs items={items} activeKey={tabKey} onChange={onTabChange} />
+        </Col>
+      </Row>
+    </FeatureFlag>
+  );
 };
 
 export default Settings;
