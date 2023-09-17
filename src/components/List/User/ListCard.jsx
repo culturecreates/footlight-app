707f1a4d6616c7fab0766cfbc02487859a3b891b
syam babu <syam@dexlock.com>
2023-09-17 23:36:09 +0530

    feat: List card for user listing page

--- src/components/List/User/ListCard.jsx
+++ src/components/List/User/ListCard.jsx
@@ -1,42 +1,12 @@
 import { List } from 'antd';
-import React, { useEffect, useState } from 'react';
-import { userActivityStatus } from '../../../constants/userActivityStatus';
-import Tags from '../../Tags/Common/Tags';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import StatusTag from '../../Tags/UserStatus/StatusTag';
 import './listCard.css';
 
 const ListCard = (props) => {
   const { id, actions, listItemHandler, title, description, activityStatus, invitedBy } = props;
-
-  const [activityStatusStyle, setActivityStatusStyle] = useState();
-  const [activityStatusTextColor, setActivityStatusTextColor] = useState();
-  const [activityStatusText, setActivityStatusText] = useState();
-
-  useEffect(() => {
-    handleStatusTagStyles();
-  }, [activityStatus]);
-
-  const handleStatusTagStyles = () => {
-    switch (activityStatus) {
-      case userActivityStatus[0].key:
-        setActivityStatusStyle({ color: '#1D8221' });
-        setActivityStatusTextColor('#DEF3D6');
-        setActivityStatusText(userActivityStatus[0].label);
-        break;
-      case userActivityStatus[1].key:
-        setActivityStatusStyle({ color: '#222732' });
-        setActivityStatusTextColor(' #E8E8E8');
-        setActivityStatusText(userActivityStatus[1].label);
-        break;
-      case userActivityStatus[2].key:
-        setActivityStatusStyle({ color: '#B59800 ' });
-        setActivityStatusTextColor('#FFF7CC');
-        setActivityStatusText(userActivityStatus[2].label);
-        break;
-      default:
-        break;
-    }
-  };
-
+  const { t } = useTranslation();
   return (
     <List.Item
       className="users-list-item-wrapper"
@@ -46,11 +16,9 @@ const ListCard = (props) => {
       onClick={listItemHandler}>
       <List.Item.Meta className="user-item-meta" title={title} description={description} />
       <div className="user-item-content">
-        <Tags color={activityStatusTextColor} style={activityStatusStyle}>
-          {activityStatusText}
-        </Tags>
+        <StatusTag activityStatus={activityStatus} />
         <div className="invitation-details">
-          <span className="invitation-details-text-prolouge">{`Invited By `}</span>
+          <span className="invitation-details-text-prolouge">{t('dashboard.settings.userManagement.invitedBy')}</span>
           <span className="invitation-details-username">{invitedBy}</span>
         </div>
       </div>
