754dac7ebaed45a77ec2414583868b6299c2d583
syam babu <syam@dexlock.com>
2023-09-18 00:00:49 +0530

    updated copy util function

--- src/utils/copyText.js
+++ src/utils/copyText.js
@@ -3,6 +3,7 @@ import { notification } from 'antd';
 export const copyText = ({ textToCopy }) => {
   navigator.clipboard.writeText(textToCopy);
   notification.success({
+    key: 'copyTextNotification',
     description: 'copied to clipboard',
     placement: 'top',
     closeIcon: <></>,
