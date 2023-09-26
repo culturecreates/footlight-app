d67a786b9649675305d0deff6917a33ed962320a
Abhishek P Anil <abhishek@culturecreates.com>
2023-09-26 17:08:27 +0530

    fix: role based check to search page routing added

--- src/pages/Dashboard/SearchPerson/SearchPerson.jsx
+++ src/pages/Dashboard/SearchPerson/SearchPerson.jsx
@@ -17,6 +17,7 @@ import { UserOutlined } from '@ant-design/icons';
 import { contentLanguageBilingual } from '../../../utils/bilingual';
 import './searchPerson.css';
 import { getArtsDataEntities } from '../../../services/artsData';
+import { routinghandler } from '../../../utils/roleRoutingHandler';
 
 function SearchPerson() {
   const { t } = useTranslation();
@@ -131,11 +132,13 @@ function SearchPerson() {
                           )
                         }
                         linkText={t('dashboard.people.createNew.search.linkText')}
-                        onClick={() =>
-                          navigate(
-                            `${PathName.Dashboard}/${calendarId}${PathName.People}${PathName.AddPerson}?id=${person?.id}`,
-                          )
-                        }
+                        onClick={() => {
+                          if (routinghandler(user, calendarId, person?.creator?.userId, null, true)) {
+                            navigate(
+                              `${PathName.Dashboard}/${calendarId}${PathName.People}${PathName.AddPerson}?id=${person?.id}`,
+                            );
+                          } else navigate(`${PathName.Dashboard}/${calendarId}${PathName.People}/${person?.id}`);
+                        }}
                       />
                     </div>
                   ))
