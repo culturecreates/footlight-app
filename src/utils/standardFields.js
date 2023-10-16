cdf08a56a957f31d8a03f574e62cd63e3c635a66
syam babu <syam@dexlock.com>
2023-10-17 00:34:21 +0530

    fix: fixed standard field mapping

--- src/utils/standardFields.js
+++ src/utils/standardFields.js
@@ -1,8 +1,20 @@
 import { taxonomyClass } from '../constants/taxonomyClass';
 
-export const standardFieldsForTaxonomy = (value) => {
+export const standardFieldsForTaxonomy = (value, takenFields) => {
+  let returnArr = [];
+  const filter = (arr, taxonomyClass) => {
+    arr.map((item) => {
+      console.log(takenFields, item);
+
+      takenFields.map((takenItem) => {
+        if (item != takenItem?.mappedToField && taxonomyClass == takenItem?.taxonomyClass) {
+          returnArr.push(item);
+        }
+      });
+    });
+  };
   if (value === taxonomyClass.EVENT) {
-    return [
+    const arr = [
       'EventAccessibility',
       'Audience',
       'EventType',
@@ -11,13 +23,23 @@ export const standardFieldsForTaxonomy = (value) => {
       'SupporterRole',
       'inLanguage',
     ];
+    filter(arr, taxonomyClass.EVENT);
+    return returnArr;
   } else if (value === taxonomyClass.PLACE) {
-    return ['PlaceAccessibility', 'Region', 'Type'];
+    const arr = ['PlaceAccessibility', 'Region', 'Type'];
+    filter(arr, taxonomyClass.PLACE);
+    return returnArr;
   } else if (value === taxonomyClass.VIRTUAL_LOCATION) {
-    return ['Type'];
+    const arr = ['Type'];
+    filter(arr, taxonomyClass.VIRTUAL_LOCATION);
+    return returnArr;
   } else if (value === taxonomyClass.PERSON) {
-    return ['Occupation'];
+    const arr = ['Occupation'];
+    filter(arr, taxonomyClass.PERSON);
+    return returnArr;
   } else {
-    return [];
+    const arr = [];
+    filter(arr, taxonomyClass.ORGANIZATION);
+    return returnArr;
   }
 };
