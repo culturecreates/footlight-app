273b76a4c919e0b5dfba493d5676a8a9d48bb11f
syam babu <syam@dexlock.com>
2023-10-31 12:21:34 +0530

    feat: updated saving modal props

--- src/components/Modal/QuickCreateSaving/QuickCreateSaving.jsx
+++ src/components/Modal/QuickCreateSaving/QuickCreateSaving.jsx
@@ -3,44 +3,47 @@ import LoadingIndicator from '../../LoadingIndicator';
 import CustomModal from '../Common/CustomModal';
 import './quickCreateSaving.css';
 
-const QuickCreateSaving = ({ open, title, text }) => {
-  console.log(title, text);
+const QuickCreateSaving = (props) => {
+  const { open, title, text } = props;
   return (
-    <CustomModal
-      open={!open}
-      width="500px"
-      height="296px"
-      destroyOnClose
-      centered
-      title={
-        <span className="quick-create-organization-modal-title" data-cy="span-quick-create-organization-heading">
-          {title}
-        </span>
-      }
-      footer={null}>
-      <div
-        style={{
-          height: '232px',
-          width: '100%',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'space-evenly',
-        }}>
-        <LoadingIndicator
+    <div className="saving-loader-modal">
+      <CustomModal
+        {...props}
+        open={!open}
+        width="500px"
+        height="296px"
+        destroyOnClose
+        centered
+        title={
+          <span className="quick-create-loader-modal-title" data-cy="span-quick-create-loader-heading">
+            {title}
+          </span>
+        }
+        footer={null}>
+        <div
           style={{
-            borderRadius: '100%',
-            background: '#EFF2FF',
-            height: 56,
-            width: 56,
+            height: '232px',
+            width: '100%',
             display: 'flex',
+            flexDirection: 'column',
             alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        />
-        <span className="text">{text}</span>
-      </div>
-    </CustomModal>
+            justifyContent: 'space-evenly',
+          }}>
+          <LoadingIndicator
+            style={{
+              borderRadius: '100%',
+              background: '#EFF2FF',
+              height: 56,
+              width: 56,
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          />
+          <span className="text">{text}</span>
+        </div>
+      </CustomModal>
+    </div>
   );
 };
 
