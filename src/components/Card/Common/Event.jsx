84bfb46c61fe2561339f3d9fd99de882539e27d0
syam babu <syam@dexlock.com>
2023-12-11 16:32:17 +0530

    fix: modified event card to accept new artsdata JSX component

--- src/components/Card/Common/Event.jsx
+++ src/components/Card/Common/Event.jsx
@@ -3,12 +3,13 @@ import { Row, Col } from 'antd';
 import './event.css';
 
 function Event(props) {
-  const { title, required, hidden } = props;
+  const { title, required, hidden, ArtsDataLinkJSX } = props;
   return (
     <Col
       className="add-event-section-col"
       flex={'780px'}
       style={{ display: hidden && 'none', borderRadius: '4px', backgroundColor: '#ffffff', margin: '0 16px' }}>
+      <Row>{ArtsDataLinkJSX}</Row>
       <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} className="events-content" justify="space-between">
         <Col flex={'423px'}>
           <div className="add-event-section-wrapper">
