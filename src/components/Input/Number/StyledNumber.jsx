86ace04f497a2005d9a7b476c247a4bbcaf0e985
Abhishek P Anil <abhishek@culturecreates.com>
2024-01-31 15:37:41 +0530

    feat: added precision two and comma for decimal separation in french

--- src/components/Input/Number/StyledNumber.jsx
+++ src/components/Input/Number/StyledNumber.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import './styledNumber.css';
 import { InputNumber } from 'antd';
+import i18n from 'i18next';
 
 function StyledNumber(props) {
-  return <InputNumber {...props} size="large" className="form-item-number-input" />;
+  return (
+    <InputNumber
+      {...props}
+      size="large"
+      className="form-item-number-input"
+      decimalSeparator={i18n?.language === 'fr' ? ',' : '.'}
+      precision={2}
+    />
+  );
 }
 
 export default StyledNumber;
