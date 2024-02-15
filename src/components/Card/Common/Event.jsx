74bcdc7151f8c17c2379ed953a6faea93575931d
syam babu <syam@dexlock.com>
2024-02-15 16:06:14 +0530

    fix: responsive design for PersonReadOnly page

--- src/components/Card/Common/Event.jsx
+++ src/components/Card/Common/Event.jsx
@@ -3,14 +3,19 @@ import { Row, Col } from 'antd';
 import './event.css';
 
 function Event(props) {
-  const { title, required, hidden, marginTop } = props;
+  const { title, required, hidden, marginTop, marginResponsive } = props;
   return (
     <Col span={24}>
       <Row>
         <Col
           className="add-event-section-col"
           flex={'780px'}
-          style={{ display: hidden && 'none', borderRadius: '4px', backgroundColor: '#ffffff', margin: '0 16px' }}>
+          style={{
+            display: hidden && 'none',
+            borderRadius: '4px',
+            backgroundColor: '#ffffff',
+            ...(marginResponsive ? { margin: marginResponsive } : { margin: '0 16px' }),
+          }}>
           <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} className="events-content" justify="space-between">
             <Col flex={'423px'}>
               <div className="add-event-section-wrapper">
