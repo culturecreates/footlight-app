a4e5e5080824d2f4ff00cc856b90bb12d0389cf4
syam babu <syam@dexlock.com>
2024-02-28 18:26:40 +0530

    fix: fixed info icons layout and interaction issues

--- src/components/ChangeType/ChangeType.jsx
+++ src/components/ChangeType/ChangeType.jsx
@@ -6,17 +6,15 @@ function ChangeType(props) {
   const { primaryIcon, disabled, label, secondaryIcon, promptText, onClick } = props;
   return (
     <div className="change-type-wrapper">
-      <TooltipStyled title={promptText}>
-        <Button
-          type="primary"
-          icon={primaryIcon}
-          disabled={disabled}
-          size="small"
-          className="first-button"
-          onClick={onClick}
-          data-cy="button-select-change-type"
-        />
-      </TooltipStyled>
+      <Button
+        type="primary"
+        icon={primaryIcon}
+        disabled={disabled}
+        size="small"
+        className="first-button"
+        onClick={onClick}
+        data-cy="button-select-change-type"
+      />
 
       <Button
         type="text"
@@ -24,15 +22,19 @@ function ChangeType(props) {
         className="second-button"
         onClick={onClick}
         data-cy="button-select-change-type">
-        {label}
-        <Button
-          type="text"
-          icon={secondaryIcon}
-          disabled={disabled}
-          size="small"
-          className="third-button"
-          data-cy="button-select-change-type"
-        />
+        <span>
+          {label}
+          <TooltipStyled title={promptText}>
+            <Button
+              type="text"
+              icon={secondaryIcon}
+              disabled={disabled}
+              size="small"
+              className="third-button"
+              data-cy="button-select-change-type"
+            />
+          </TooltipStyled>
+        </span>
       </Button>
     </div>
   );
