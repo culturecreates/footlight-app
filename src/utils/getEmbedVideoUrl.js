d9c2741a690d84a596e2b47ab2f97bd3b8375abc
syam babu <syam@dexlock.com>
2024-03-12 17:24:42 +0530

    fix: added embedd url to validation

--- src/utils/getEmbedVideoUrl.js
+++ src/utils/getEmbedVideoUrl.js
@@ -1,6 +1,13 @@
 /* eslint-disable no-useless-escape */
 /* eslint-disable no-undef */
 export function getEmbedUrl(url) {
+  const embedUrlRegex = /(?:https?:)?\/\/(?:www\.)?(?:youtube\.com\/embed\/|player\.vimeo\.com\/video\/)(.+)/;
+  const embedUrlMatch = embedUrlRegex.exec(url);
+
+  if (embedUrlMatch && (validateYouTubeURL(url) || validateVimeoURL(url))) {
+    return url;
+  }
+
   const youtubeRegex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?(.+)/;
   const vimeoRegex = /(?:https?:\/\/)?(?:www\.)?vimeo\.com\/(.+)/;
 
@@ -8,9 +15,10 @@ export function getEmbedUrl(url) {
   const vimeoMatch = vimeoRegex.exec(url);
 
   let embedUrl;
-  if (youtubeMatch) {
+
+  if (youtubeMatch && validateYouTubeURL(url)) {
     embedUrl = `https://www.youtube.com/embed/${youtubeMatch[1]}`;
-  } else if (vimeoMatch) {
+  } else if (vimeoMatch && validateVimeoURL(url)) {
     embedUrl = `//player.vimeo.com/video/${vimeoMatch[1]}`;
   } else {
     return '';
@@ -20,11 +28,12 @@ export function getEmbedUrl(url) {
 }
 
 export const validateYouTubeURL = (url) => {
-  const youtubeRegex = /^((https?\:)?\/\/)?((www\.)?youtube\.com|youtu\.be)\/watch\?(v=[^&\s]+)/;
-  return youtubeRegex.test(url);
+  let pattern =
+    /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
+  return pattern.test(url);
 };
 
 export const validateVimeoURL = (url) => {
-  const vimeoRegex = /^((https?\:)?\/\/)?(player\.)?vimeo\.com\/([0-9]+)/;
+  const vimeoRegex = /^((https?\:)?\/\/)?(player\.)?vimeo\.com\/(?:video\/)?([0-9]+)/;
   return vimeoRegex.test(url);
 };
