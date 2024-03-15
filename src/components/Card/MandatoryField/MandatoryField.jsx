20ebc5d1aa539d36e52c34948b98c5ead64fddeb
Abhishek P Anil <abhishek@culturecreates.com>
2024-03-15 11:24:45 +0530

    feat: added the logic to add and remove required fields.Pausing and start to work in 780

--- src/components/Card/MandatoryField/MandatoryField.jsx
+++ src/components/Card/MandatoryField/MandatoryField.jsx
@@ -1,12 +1,80 @@
-import React from 'react';
-import { Card } from 'antd';
+import React, { useState } from 'react';
+import { Card, Col, Divider, Row } from 'antd';
+import { bilingual } from '../../../utils/bilingual';
+import { useSelector } from 'react-redux';
+import { getUserDetails } from '../../../redux/reducer/userSlice';
 
-function MandatoryField() {
+function MandatoryField(props) {
+  const { field } = props;
+  const { user } = useSelector(getUserDetails);
+
+  const [addedFields, setAddedFields] = useState(field?.filter((f) => f?.isRequiredField));
+  const [availableFields, setAvailableFields] = useState(field?.filter((f) => !f?.isRequiredField));
+
+  const removeFromFields = (index) => {
+    let removedField = addedFields[index];
+    removedField = {
+      ...removedField,
+      isRequiredField: false,
+    };
+    if (!removedField?.preFilled) {
+      const updatedFields = addedFields.filter((field, i) => i !== index);
+      setAddedFields(updatedFields);
+      setAvailableFields([...availableFields, removedField]);
+    }
+  };
+
+  const addToFields = (field) => {
+    setAddedFields([...addedFields, { ...field, isRequiredField: true }]);
+    let updatedFields = availableFields.filter((f) => f?.mappedField !== field?.mappedField);
+    updatedFields = updatedFields?.map((f) => {
+      return {
+        ...f,
+        isRequiredField: false,
+      };
+    });
+    setAvailableFields(updatedFields);
+  };
+
   return (
     <Card>
-      <p>Card content</p>
-      <p>Card content</p>
-      <p>Card content</p>
+      <Row gutter={[0, 18]}>
+        <Col span={24}>
+          <h5>General Settings</h5>
+        </Col>
+        <Col span={11}>
+          <h5>General Settings</h5>
+          <ul>
+            {addedFields.map((field, index) => (
+              <li key={index}>
+                {bilingual({
+                  en: field?.label?.en,
+                  fr: field?.label?.fr,
+                  interfaceLanguage: user?.interfaceLanguage?.toLowerCase(),
+                })}
+                {!field?.preFilled && <button onClick={() => removeFromFields(index)}>Remove</button>}
+              </li>
+            ))}
+          </ul>
+        </Col>
+        <Col>
+          <Divider type="vertical" style={{ height: '100%' }} />
+        </Col>
+        <Col span={11}>
+          <h5>General Settings</h5>
+          <ul>
+            {availableFields.map((field, index) => (
+              <li key={index} onClick={() => addToFields(field)}>
+                {bilingual({
+                  en: field?.label?.en,
+                  fr: field?.label?.fr,
+                  interfaceLanguage: user?.interfaceLanguage?.toLowerCase(),
+                })}
+              </li>
+            ))}
+          </ul>
+        </Col>
+      </Row>
     </Card>
   );
 }
--- src/pages/Dashboard/Settings/MandatoryFields/MandatoryFields.jsx
+++ src/pages/Dashboard/Settings/MandatoryFields/MandatoryFields.jsx
@@ -2,28 +2,33 @@ import React from 'react';
 import { Row, Col } from 'antd';
 import MandatoryFieldCard from '../../../../components/Card/MandatoryField/MandatoryField';
 import { useTranslation } from 'react-i18next';
+import { useOutletContext } from 'react-router-dom';
 
 function MandatoryFields() {
   const { t } = useTranslation();
+  const [currentCalendarData] = useOutletContext();
 
+  const fields = currentCalendarData?.forms;
   return (
-    <Row gutter={[0, 18]}>
-      <Col span={24}>
-        <h5> {t('dashboard.settings.calendarSettings.generalSettings')}</h5>
-      </Col>
-      <Col span={24}>
-        <p>{t('dashboard.settings.calendarSettings.setUpCalendarDescription')}</p>
-      </Col>
-      <Col flex={'576px'}>
-        <Row gutter={[8, 18]}>
-          {[0, 1, 2, 3].map((_, index) => (
-            <Col span={24} key={index}>
-              <MandatoryFieldCard />
-            </Col>
-          ))}
-        </Row>
-      </Col>
-    </Row>
+    currentCalendarData && (
+      <Row gutter={[0, 18]}>
+        <Col span={24}>
+          <h5> {t('dashboard.settings.calendarSettings.generalSettings')}</h5>
+        </Col>
+        <Col span={24}>
+          <p>{t('dashboard.settings.calendarSettings.setUpCalendarDescription')}</p>
+        </Col>
+        <Col flex={'576px'}>
+          <Row gutter={[8, 18]}>
+            {fields.map((field, index) => (
+              <Col span={24} key={index}>
+                <MandatoryFieldCard field={field?.formFields} />
+              </Col>
+            ))}
+          </Row>
+        </Col>
+      </Row>
+    )
   );
 }
 
