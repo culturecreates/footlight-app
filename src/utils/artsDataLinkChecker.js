9dfe55a8d4359b8164dad61e100173e48ad80a73
Abhishek P Anil <abhishek@culturecreates.com>
2024-03-21 15:59:21 +0530

    refactor: closes #1032

--- src/utils/artsDataLinkChecker.js
+++ src/utils/artsDataLinkChecker.js
@@ -2,7 +2,6 @@ import { sameAsTypes } from '../constants/sameAsTypes';
 
 export const artsDataLinkChecker = (link) => {
   const artsData = 'artsdata';
-
   if (Array.isArray(link)) {
     const artsDataLink = link?.find((item) => item?.type === sameAsTypes.ARTSDATA_IDENTIFIER)?.uri;
     if (artsDataLink) {
