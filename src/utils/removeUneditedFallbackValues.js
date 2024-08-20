4aebf1bd46efdacbaf7310bfbfe1b946022ecb21
syam babu <syam@dexlock.com>
2024-08-20 17:20:08 +0530

    fix: added new fallback filtering function

--- src/utils/removeUneditedFallbackValues.js
+++ src/utils/removeUneditedFallbackValues.js
@@ -1,11 +1,29 @@
-export const removeUneditedFallbackValues = ({ values, activeFallbackFieldsInfo, fieldName, property }) => {
-  if (values == '' || !values) {
-    return;
-  }
-  if (!Object.prototype.hasOwnProperty.call(activeFallbackFieldsInfo, fieldName)) {
-    return values;
-  }
-  if (activeFallbackFieldsInfo[fieldName][property]?.tagDisplayStatus) {
-    return;
-  } else return values;
+export const filterUneditedFallbackValues = ({ values, activeFallbackFieldsInfo = {}, fieldName }) => {
+  let requiredFallbackKeyForCurrentField;
+  if (!(Object.keys(activeFallbackFieldsInfo).length > 0)) return values;
+
+  Object.keys(activeFallbackFieldsInfo).forEach((key) => {
+    const firstItemName = key.split('-');
+    const fieldNameFlag = firstItemName[0].slice(0, -2);
+
+    if (fieldName === fieldNameFlag) requiredFallbackKeyForCurrentField = key;
+  });
+  if (!requiredFallbackKeyForCurrentField) return values;
+
+  let modifiedValues = {};
+
+  const requiredFallbackStatusForCurrentField = activeFallbackFieldsInfo[requiredFallbackKeyForCurrentField];
+  Object.keys(values).forEach((key) => {
+    if (!Object.hasOwn(requiredFallbackStatusForCurrentField, key)) {
+      modifiedValues[key] = values[key]?.trim();
+    }
+  });
+
+  Object.keys(requiredFallbackStatusForCurrentField).forEach((key) => {
+    if (!requiredFallbackStatusForCurrentField[key]?.tagDisplayStatus) {
+      modifiedValues[key] = requiredFallbackStatusForCurrentField[key]?.fallbackLiteralValue?.trim();
+    }
+  });
+
+  return modifiedValues;
 };
