6dfe036404c6be6cc47e60ffe06c0d026dc6cbc3
Abhishek P Anil <abhishek@culturecreates.com>
2024-10-07 12:00:38 +0530

    feat:  tried use

--- src/components/MapComponent/MapComponent.jsx
+++ src/components/MapComponent/MapComponent.jsx
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { GoogleMap, Marker } from '@react-google-maps/api';
 
-const MapComponent = () => {
+const MapComponent = (props) => {
+  const { latitude, longitude } = props;
+
   const mapStyles = {
     height: '400px',
     width: '100%',
   };
 
+  // Provide default values if latitude and longitude props are not passed
   const defaultCenter = {
-    lat: 45.421532,
-    lng: -75.697189,
+    lat: !isNaN(parseFloat(latitude)) ? parseFloat(latitude) : 45.421532,
+    lng: !isNaN(parseFloat(longitude)) ? parseFloat(longitude) : -75.697189,
+  };
+
+  const [selectedPosition, setSelectedPosition] = useState(defaultCenter);
+
+  useEffect(() => {
+    // Update selectedPosition if props change
+    setSelectedPosition({
+      lat: !isNaN(parseFloat(latitude)) ? parseFloat(latitude) : 45.421532,
+      lng: !isNaN(parseFloat(longitude)) ? parseFloat(longitude) : -75.697189,
+    });
+  }, [latitude, longitude]);
+
+  const onMapClick = (e) => {
+    const newPosition = {
+      lat: e.latLng.lat(),
+      lng: e.latLng.lng(),
+    };
+    setSelectedPosition(newPosition);
   };
 
   return (
-    <GoogleMap mapContainerStyle={mapStyles} zoom={13} center={defaultCenter}>
-      <Marker position={defaultCenter} />
+    <GoogleMap mapContainerStyle={mapStyles} zoom={13} center={selectedPosition} onClick={onMapClick}>
+      {/* Render the marker when selectedPosition is valid */}
+      {selectedPosition && <Marker position={selectedPosition} draggable={true} />}
     </GoogleMap>
   );
 };
