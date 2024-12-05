3bb50d09a7d5a0547aa3accef9628fa7b9a7c6a0
Abhishek P Anil <abhishek@culturecreates.com>
2024-12-05 16:02:41 +0530

    feat: udpated default exports

--- src/components/Card/Common/index.js
+++ src/components/Card/Common/index.js
@@ -1,1 +1,4 @@
 export { default } from './Event';
+export { default as ReviewContent } from './ReviewContent';
+export { default as EntityCard } from './EntityCard';
+export { default as CreateEntityButton } from './CreateEntityButton';
