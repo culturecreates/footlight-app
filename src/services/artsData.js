6371a41c73361c1dc117d9a4c85f530f9578d082
syam babu <syam@dexlock.com>
2025-07-28 18:16:33 +0530

    chore: Update artsData service to use new query format. closes #1622

--- src/services/artsData.js
+++ src/services/artsData.js
@@ -15,25 +15,22 @@ export async function getArtsDataEntities({ searchKeyword, entityType }) {
 }
 
 export async function loadArtsDataEntity({ entityId }) {
-  const sparqlQuery =
-    'https://gist.githubusercontent.com/dev-aravind/809b63b291fa7eadfb5a2bd97144d9ee/raw/9c919a5802394be8db2500624327a58da1166385/ranked_org_person_footlight.sparql';
-  const query = `query?adid=${entityId}&format=json&frame=ranked_org_person_footlight&sparql=${sparqlQuery}`;
+  const sparqlQuery = 'footlight_cms/org_person';
+  const query = `query?uri=${entityId}&format=json&frame=ranked_org_person_footlight&sparql=${sparqlQuery}`;
   const url = `${baseUrl}/${query}`;
   return fetchData(url);
 }
 export async function loadArtsDataPlaceEntity({ entityId }) {
-  const sparqlQuery =
-    'https://gist.githubusercontent.com/dev-aravind/f7b4b7cfc5eafddf9f7d67287bea6d9e/raw/762bef0d6ea27bed33f1df4ae7985e22f4449874/ranked_place_footlight.sparql';
-  const query = `query?adid=${entityId}&format=json&frame=ranked_place_footlight&sparql=${sparqlQuery}`;
+  const sparqlQuery = 'footlight_cms/place';
+  const query = `query?uri=${entityId}&format=json&frame=ranked_place_footlight&sparql=${sparqlQuery}`;
   const url = `${baseUrl}/${query}`;
   return fetchData(url);
 }
 
 export async function loadArtsDataEventEntity({ entityId }) {
-  const sparqlQuery =
-    'https://gist.githubusercontent.com/dev-aravind/da9311f07d419e3bde07d0b826c7480f/raw/febc219d26d9997b8c3210e07be6ce268960ad9f/ranked_event_footlight.sparql';
+  const sparqlQuery = 'footlight_cms/event';
 
-  const query = `query?adid=${entityId}&format=json&frame=ranked_event_footlight&sparql=${sparqlQuery}`;
+  const query = `query?uri=${entityId}&format=json&frame=ranked_event_footlight&sparql=${sparqlQuery}`;
   const url = `${baseUrl}/${query}`;
 
   return fetchData(url);
