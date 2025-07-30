792f718ca43464f1a8a2d45da866a08687f83606
syam babu <syam@dexlock.com>
2025-07-30 16:45:31 +0530

    feat: added notification services

---
+++ src/services/notification.js
@@ -0,0 +1,40 @@
+import { createApi } from '@reduxjs/toolkit/query/react';
+import { baseQueryWithReauth } from '../utils/services';
+import { messageStatusMap } from '../constants/notificationConstants';
+
+export const notificationApi = createApi({
+  reducerPath: 'notificationApi',
+  baseQuery: baseQueryWithReauth,
+  endpoints: (builder) => ({
+    getNotifications: builder.query({
+      query: ({ calendarId, sinceNdays }) => ({
+        url: 'messages',
+        headers: {
+          'calendar-id': calendarId,
+          'since-n-days': sinceNdays,
+        },
+      }),
+      transformResponse: (response) => {
+        const unreadCount = response?.data?.filter((item) => item.messageStatus === messageStatusMap.SENT).length || 0;
+        return {
+          notifications: response?.data,
+          unreadCount,
+          lastUpdated: new Date().toISOString(),
+        };
+      },
+
+      keepUnusedDataFor: 30,
+    }),
+    markAsRead: builder.mutation({
+      query: ({ calendarId, messageId }) => ({
+        url: `messages/${messageId}/mark-as-read`,
+        method: 'PATCH',
+        headers: {
+          'calendar-id': calendarId,
+        },
+      }),
+    }),
+  }),
+});
+
+export const { useGetNotificationsQuery, useLazyGetNotificationsQuery, useMarkAsReadMutation } = notificationApi;
