1544b393763f548e938f1a36f14edc72905deee3
syam babu <syam@dexlock.com>
2025-07-31 17:01:03 +0530

    feat: added read only page for system updates notifications

--- src/pages/Dashboard/Messages/SystemUpdates/SystemUpdates.jsx
+++ src/pages/Dashboard/Messages/SystemUpdates/SystemUpdates.jsx
@@ -1,18 +1,37 @@
 import React from 'react';
 import { useGetNotificationsQuery } from '../../../../services/notification';
-import { useParams } from 'react-router-dom';
-import { Typography, Divider, Spin, Row, Col } from 'antd';
+import { useOutletContext, useParams } from 'react-router-dom';
+import { Typography, Spin, Row, Col, Card } from 'antd';
 import { useTranslation } from 'react-i18next';
 import { messageTypeMap } from '../../../../constants/notificationConstants';
-import dayjs from 'dayjs';
 import { bilingual } from '../../../../utils/bilingual';
 import i18next from 'i18next';
+import moment from 'moment';
+import { useSelector } from 'react-redux';
+import { getUserDetails } from '../../../../redux/reducer/userSlice';
+import './systemUpdates.css';
 
 const { Title, Text } = Typography;
 
 const SystemUpdates = () => {
   const { calendarId } = useParams();
+  const { user } = useSelector(getUserDetails);
+
+  const isSuperAdmin = !user?.role?.length;
+
   const { t } = useTranslation();
+  const [
+    currentCalendarData, // eslint-disable-next-line no-unused-vars
+    _pageNumber, // eslint-disable-next-line no-unused-vars
+    _setPageNumber, // eslint-disable-next-line no-unused-vars
+    _getCalendar,
+    setContentBackgroundColor,
+  ] = useOutletContext();
+
+  setContentBackgroundColor('#F9FAFF');
+
+  const { timezone } = currentCalendarData;
+
   const { currentData: data, isLoading } = useGetNotificationsQuery(
     { calendarId, messageType: messageTypeMap.SYSTEM },
     { refetchOnMountOrArgChange: true },
@@ -22,46 +41,62 @@ const SystemUpdates = () => {
   const interfaceLanguage = i18next.language || 'en';
 
   const formatDate = (dateString) => {
-    return dayjs(dateString).format('D MMMM YYYY');
+    return moment.tz(dateString, timezone).locale(interfaceLanguage).format('DD-MMM-YYYY')?.toUpperCase();
+  };
+
+  const onClickHandler = (e) => {
+    if (isSuperAdmin) {
+      console.log('clicked on notification:', e.currentTarget);
+    }
   };
 
   return (
-    <div className="system-updates-container">
-      <Title level={2} className="system-updates-title">
+    <div className="system-updates-container" style={{ padding: '24px' }}>
+      <Title level={2} className="system-updates-title" style={{ marginBottom: '32px' }}>
         {t('notification.systemUpdates.title')}
       </Title>
-      <Divider />
 
       {isLoading ? (
         <Spin size="large" />
       ) : notifications.length === 0 ? (
         <Text type="secondary">{t('notification.systemUpdates.empty')}</Text>
       ) : (
-        <div className="updates-list">
-          {notifications.map((notification) => (
-            <Row key={notification.id} gutter={16} className="update-item">
-              <Col xs={24} sm={6} md={5} lg={4}>
-                <Text strong className="update-date">
-                  {formatDate(notification.createdAt)}
-                </Text>
-              </Col>
-              <Col xs={24} sm={18} md={19} lg={20}>
-                <div className="update-content">
-                  {notification.title && (
-                    <Title level={4} className="update-title">
-                      {bilingual({ interfaceLanguage, data: notification.title })}
-                    </Title>
-                  )}
-                  <div className="update-description">
-                    <Text strong>{t('notification.systemUpdates.descriptionLabel')}:</Text>
-                    <Text>{bilingual({ interfaceLanguage, data: notification?.message })}</Text>
-                  </div>
-                </div>
-              </Col>
-              <Divider />
-            </Row>
-          ))}
-        </div>
+        <Row className="updates-list">
+          <Col span={24} flex="748px">
+            <Card bordered={false} className="updates-card">
+              {notifications.map((notification) => {
+                return (
+                  <Row
+                    key={notification.id}
+                    className={`update-item ${isSuperAdmin ? 'hoverable' : ''}`}
+                    onClick={onClickHandler}
+                    gutter={16}>
+                    <div
+                      style={{
+                        width: '150px',
+                        flexShrink: 0,
+                      }}>
+                      <Text strong className="update-date">
+                        {formatDate(notification.messageTime)}
+                      </Text>
+                    </div>
+
+                    <div style={{ flex: 1 }}>
+                      {notification.messageHeading && (
+                        <Title level={5} className="update-title">
+                          {bilingual({ interfaceLanguage, data: notification.messageHeading })}
+                        </Title>
+                      )}
+                      <div className="update-description" style={{ fontWeight: 400 }}>
+                        <Text>{bilingual({ interfaceLanguage, data: notification?.messageDescription })}</Text>
+                      </div>
+                    </div>
+                  </Row>
+                );
+              })}
+            </Card>
+          </Col>
+        </Row>
       )}
     </div>
   );
