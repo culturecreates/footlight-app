fac417f345e26384852bf9755caa3b6def3f0194
syam babu <syam@dexlock.com>
2025-08-05 13:54:24 +0530

    fix: fixed base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  base: '/build/',
+  base: '/',
   plugins: [
     react({
       babel: {
