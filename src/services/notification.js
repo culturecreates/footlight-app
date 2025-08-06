4e68ee754bfbabc0d60da721e2ee3b2ddae774be
syam babu <syam@dexlock.com>
2025-08-06 15:20:30 +0530

    feat: refactored notification api

--- src/services/notification.js
+++ src/services/notification.js
@@ -7,14 +7,25 @@ export const notificationApi = createApi({
   baseQuery: baseQueryWithReauth,
   endpoints: (builder) => ({
     getNotifications: builder.query({
-      query: ({ calendarId, sinceNdays, messageType }) => ({
-        url: 'messages',
-        headers: {
-          'calendar-id': calendarId,
-          ...(sinceNdays ? { 'since-n-days': sinceNdays } : {}),
-          ...(messageType ? { 'message-type': messageType } : {}),
-        },
-      }),
+      query: ({ calendarId, sinceNdays, messageType }) => {
+        let url = `messages`;
+
+        const params = new URLSearchParams();
+        if (messageType) params.append('message-type', messageType);
+        if (sinceNdays) params.append('since-n-days', sinceNdays);
+
+        if (params.toString()) {
+          url += `?${params.toString()}`;
+        }
+
+        return {
+          url,
+          headers: {
+            'calendar-id': calendarId,
+          },
+        };
+      },
+
       transformResponse: (response) => {
         const unreadCount = response?.data?.filter((item) => item.messageStatus === messageStatusMap.SENT).length || 0;
         return {
@@ -35,7 +46,22 @@ export const notificationApi = createApi({
         },
       }),
     }),
+    createNotification: builder.mutation({
+      query: ({ calendarId, notificationData }) => ({
+        url: 'messages',
+        method: 'POST',
+        headers: {
+          'calendar-id': calendarId,
+        },
+        body: notificationData,
+      }),
+    }),
   }),
 });
 
-export const { useGetNotificationsQuery, useLazyGetNotificationsQuery, useMarkAsReadMutation } = notificationApi;
+export const {
+  useGetNotificationsQuery,
+  useLazyGetNotificationsQuery,
+  useMarkAsReadMutation,
+  useCreateNotificationMutation,
+} = notificationApi;
