ccc598585d092866c25fd6e3858cb52dd8244556
syam babu <syam@dexlock.com>
2025-08-22 16:51:38 +0530

    feat: Use CustomModal for duplicate entity import confirmation in SearchPlaces component. closes #1864

--- src/pages/Dashboard/SearchPlaces/SearchPlaces.jsx
+++ src/pages/Dashboard/SearchPlaces/SearchPlaces.jsx
@@ -23,7 +23,7 @@ import { useLazyGetExternalSourceQuery } from '../../../services/externalSource'
 import LoadingIndicator from '../../../components/LoadingIndicator';
 import { externalSourceOptions } from '../../../constants/sourceOptions';
 import useAbortControllersOnUnmount from '../../../hooks/useAbortControllersOnUnmount';
-import { Confirm } from '../../../components/Modal/Confirm/Confirm';
+import { CustomModal } from '../../../components/Modal/HookModal/Modal';
 
 function SearchPlaces() {
   const { t } = useTranslation();
@@ -81,13 +81,15 @@ function SearchPlaces() {
 
   const confirmPopupHandler = (fn, entity) => {
     if (entity?.footlightId) {
-      Confirm({
+      CustomModal({
         title: t('dashboard.events.createNew.search.confirm.title'),
         content: t('dashboard.events.createNew.search.confirm.content'),
-        okText: t('dashboard.events.createNew.search.confirm.okText'),
+        primaryButtonText: t('dashboard.events.createNew.search.confirm.editText'),
+        secondaryButtonText: t('dashboard.events.createNew.search.confirm.okText'),
         cancelText: t('dashboard.events.createNew.search.confirm.cancelText'),
-        onAction: () => fn(),
-        onCancel: () => {
+        className: 'existing-entity-found-info-modal',
+        secondaryAction: () => fn(),
+        primaryAction: () => {
           navigate(
             `${PathName.Dashboard}/${calendarId}${PathName.Organizations}${PathName.AddOrganization}?id=${entity?.footlightId}`,
           );
@@ -283,6 +285,7 @@ function SearchPlaces() {
                                   const fn = () => {
                                     navigate(
                                       `${PathName.Dashboard}/${calendarId}${PathName.Places}${PathName.AddPlace}?entityId=${place?.id}`,
+                                      { state: { data: { footlightId: place?.footlightId } } },
                                     );
                                   };
                                   confirmPopupHandler(fn, place);
