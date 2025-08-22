3018551fa0946b5528bb389d18ef5ddeae6545c3
syam babu <syam@dexlock.com>
2025-08-22 16:37:16 +0530

    feat: changed the duplicate entity modal to use customHook modal in SearchPerson.jsx

--- src/pages/Dashboard/SearchPerson/SearchPerson.jsx
+++ src/pages/Dashboard/SearchPerson/SearchPerson.jsx
@@ -23,7 +23,7 @@ import { useLazyGetExternalSourceQuery } from '../../../services/externalSource'
 import LoadingIndicator from '../../../components/LoadingIndicator';
 import { externalSourceOptions } from '../../../constants/sourceOptions';
 import useAbortControllersOnUnmount from '../../../hooks/useAbortControllersOnUnmount';
-import { Confirm } from '../../../components/Modal/Confirm/Confirm';
+import { CustomModal } from '../../../components/Modal/HookModal/Modal';
 
 function SearchPerson() {
   const { t } = useTranslation();
@@ -83,13 +83,15 @@ function SearchPerson() {
 
   const confirmPopupHandler = (fn, entity) => {
     if (entity?.footlightId) {
-      Confirm({
+      CustomModal({
         title: t('dashboard.events.createNew.search.confirm.title'),
         content: t('dashboard.events.createNew.search.confirm.content'),
-        okText: t('dashboard.events.createNew.search.confirm.okText'),
+        primaryButtonText: t('dashboard.events.createNew.search.confirm.editText'),
+        secondaryButtonText: t('dashboard.events.createNew.search.confirm.okText'),
         cancelText: t('dashboard.events.createNew.search.confirm.cancelText'),
-        onAction: () => fn(),
-        onCancel: () => {
+        className: 'existing-entity-found-info-modal',
+        secondaryAction: () => fn(),
+        primaryAction: () => {
           navigate(
             `${PathName.Dashboard}/${calendarId}${PathName.Organizations}${PathName.AddOrganization}?id=${entity?.footlightId}`,
           );
@@ -284,6 +286,7 @@ function SearchPerson() {
                                 const fn = () => {
                                   navigate(
                                     `${PathName.Dashboard}/${calendarId}${PathName.People}${PathName.AddPerson}?entityId=${person?.id}`,
+                                    { state: { data: { footlightId: person?.footlightId } } },
                                   );
                                 };
                                 confirmPopupHandler(fn, person);
